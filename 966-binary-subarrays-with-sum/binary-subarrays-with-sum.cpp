a25a9fd11d7eedc90364fea71068da6de7c2b7b5
Saisha Verma <saishaverma0512@gmail.com>
2025-09-13 02:47:48 +0530

    Time: 20 ms (26.07%) | Memory: 38.8 MB (18.78%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -3,11 +3,12 @@ public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
         int pre = 0, cnt = 0;
         unordered_map<int, int> freq;
+        freq[0] = 1;
         for (int i = 0; i < nums.size(); i ++){
             pre += nums[i];
-
-            if (pre == goal)    cnt ++;
-            if (freq.find(pre - goal) != freq.end())    cnt += freq[pre - goal];
+            if (freq.find(pre - goal) != freq.end()){
+                cnt += freq[pre - goal];
+            }
             freq[pre] ++;
         }
         return cnt;
