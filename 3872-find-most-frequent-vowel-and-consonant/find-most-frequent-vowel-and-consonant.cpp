f2d6234927572c7aa52a66d2329f371ed3544885
Saisha Verma <saishaverma0512@gmail.com>
2025-09-14 02:44:18 +0530

    Time: 1 ms (53.72%) | Memory: 9.8 MB (44.68%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int maxFreqSum(string s) {
-        map<char, int> freq;
-        int maxvow = 0, maxcons = 0;
+        unordered_map<char, int> freq;
         for (int i = 0; i < s.size(); i ++){
             freq[s[i]] ++;
+        }
 
-            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u'){
-                if (freq[s[i]] > maxvow){
-                    maxvow = freq[s[i]];
-                }
+        int maxvow = 0, maxcons = 0;
+        for (auto &p : freq){
+            char ch = p.first;
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                if (p.second > maxvow)  maxvow = p.second;
             }
-            else {
-                if (freq[s[i]] > maxcons){
-                    maxcons = freq[s[i]];
-                }
+            else if (ch >= 'a' && ch <= 'z'){
+                if (p.second > maxcons) maxcons = p.second;
             }
         }
-        return maxvow + maxcons;
+
+        return maxcons + maxvow;
     }
 };
