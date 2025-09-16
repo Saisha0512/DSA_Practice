452d13a28ffc1bdc8624cff2b2d8f354f481d472
Saisha Verma <saishaverma0512@gmail.com>
2025-09-16 18:30:25 +0530

    Time: 20 ms (58.80%) | Memory: 125.2 MB (64.81%) - LeetSync

--- 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.cpp
+++ 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.cpp
@@ -1,34 +1,14 @@
 class Solution {
 public:
-
-    long long gcd(long long a, long long b){
-        while(b != 0){
-            long long temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
-    }
-
-    long long lcm(long long a, long long b){
-        return (a / gcd(a, b)) * b;
-    }
-
     vector<int> replaceNonCoprimes(vector<int>& nums) {
-        int i = 0; 
-        stack<long long> s;
-        for (auto &num : nums){
-            long long curr = num;
-            while (!s.empty() && gcd(s.top(), curr) > 1){
-                curr = lcm(s.top(), curr);
-                s.pop();
+        vector<int> res;
+        for (int num : nums){
+            int curr = num;
+            while (!res.empty() && gcd(res.back(), curr) > 1){
+                curr = lcm(curr, res.back());
+                res.pop_back();
             }
-            s.push(curr);
-        }
-        vector<int> res(s.size());
-        for (int i = s.size() - 1; i >= 0; i --){
-            res[i] = (int)s.top();
-            s.pop();
+            res.push_back(curr);
         }
         return res;
     }
