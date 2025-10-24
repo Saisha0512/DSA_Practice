55b4d1ceb63b7163f57f7e8ab855a81f8d73275f
Saisha Verma <saishaverma0512@gmail.com>
2025-10-25 00:34:39 +0530

    Time: 9 ms (88.99%) | Memory: 34.8 MB (67.20%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -18,13 +18,11 @@ public:
         while (!q.empty()){
             auto [x, y] = q.front();
             q.pop();
-            int dist = res[x][y];
 
             for (auto [dx, dy] : dirs){
                 int i = x + dx, j = y + dy;
-
                 if (i >= 0 && j >= 0 && i < n && j < m && res[i][j] == -1){
-                    res[i][j] = dist + 1;
+                    res[i][j] = res[x][y] + 1;
                     q.push({i, j});
                 }
             }
