08b6c93ef8a879967a4a762a676c293f67a74223
Saisha Verma <saishaverma0512@gmail.com>
2025-10-24 23:52:17 +0530

    Time: 1 ms (22.07%) | Memory: 18.5 MB (23.12%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int n = image.size(), m = image[0].size();
-        int original = image[sr][sc]; // Original Color
-        // If the starting pixel is the color itself : 
-        if (original == color){
+        if (image[sr][sc] == color){
             return image;
         }
-
-        // Otherwise : 
-        vector<pair<int, int>> dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}}; // 4 - Directions to move
+        
+        int n = image.size(), m = image[0].size();
+        int org = image[sr][sc];
         queue<pair<int, int>> q;
+        image[sr][sc] = color;
         q.push({sr, sc});
+
+        vector<pair<int, int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
         while (!q.empty()){
             auto [x, y] = q.front();
-            // Making the current pixel = col
-            image[x][y] = color;
             q.pop();
+
             for (auto [dx, dy] : dirs){
                 int i = x + dx, j = y + dy;
-                if (i >= 0 && i < n && j >= 0 && j < m && image[i][j] == original){ // The color of the current node should be same as the original color
+                if (i >= 0 && j >= 0 && i < n && j < m && image[i][j] == org){
+                    image[i][j] = color;
                     q.push({i, j});
                 }
             }
         }
+
         return image;
     }
 };
