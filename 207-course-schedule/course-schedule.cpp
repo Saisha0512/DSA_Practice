9d789bd74babc1f2ea70da22ef8ce1812ee794aa
Saisha Verma <saishaverma0512@gmail.com>
2025-11-03 13:37:25 +0530

    Time: 4 ms (67.17%) | Memory: 19.1 MB (85.33%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& pre) {
+        vector<vector<int>> graph(n);
+        vector<int> indegree(n, 0);
+
+        for(int i = 0; i < pre.size(); i ++){
+            int a = pre[i][0], b = pre[i][1];
+            graph[b].push_back(a);
+            indegree[a] ++;
+        }
+
+        queue<int> q;
+        for (int i = 0; i < n; i ++){
+            if (indegree[i] == 0){
+                q.push(i);
+            }
+        }
+
+        int done = 0;
+        while (!q.empty()){
+            int curr = q.front();
+            q.pop();
+            done ++;
+
+            for (int nbr : graph[curr]){
+                indegree[nbr] --;
+                if (indegree[nbr] == 0){
+                    q.push(nbr);
+                }
+            }
+        }
+
+        return (done == n);
+    }
+};
