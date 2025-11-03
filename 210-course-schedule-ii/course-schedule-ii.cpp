a8d029ba50dd57c868b8489ac4741afba99fdba7
Saisha Verma <saishaverma0512@gmail.com>
2025-11-03 23:51:13 +0530

    Time: 9 ms (17.30%) | Memory: 18.7 MB (46.94%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,42 +1,47 @@
-// BFS Approach - Kahn's Algorithm : 
+// DFS Approach : 
 class Solution {
-public:
-    vector<int> findOrder(int n, vector<vector<int>>& pre) {
-        vector<vector<int>> graph(n);
-        vector<int> indegree(n, 0);
+    bool dfs(vector<vector<int>> &graph, vector<int> &vis, list<int> &order, int node){
+        // Visiting the current node : 
+        vis[node] = 1;
 
-        // Adjacency List for the graph : 
-        for (auto &p : pre){
-            graph[p[1]].push_back(p[0]);
-            indegree[p[0]] ++;
-        }
-
-        // Checking for nodes with indegree = 0 & push them into a queue : 
-        queue<int> q;
-        for (int i = 0; i < n; i ++){
-            if(indegree[i] == 0){
-                q.push(i);
+        // Iterating over all the neighbours : 
+        for (auto nbr : graph[node]){
+            if (vis[nbr] == 1){ // nbr is in the current path already - Cycle Detected
+                return false;
             }
-        }
-        
-        // BFS Kahn's Algo :
-        vector<int> order; 
-        while (!q.empty()){
-            int node = q.front();
-            q.pop();
-            order.push_back(node);
-
-            for (auto nbr : graph[node]){
-                indegree[nbr] --;
-                if (indegree[nbr] == 0){
-                    q.push(nbr);
+            if (vis[nbr] == 0){
+                if (dfs(graph, vis, order, nbr) == false){
+                    return false;
                 }
             }
         }
 
-        if (order.size() == n){
-            return order;
+        // After visiting & adding all the neighbours of the current node to the order, 
+        // we add the current node to the order list from the front : 
+        vis[node] = 2;
+        order.push_front(node);
+        return true;
+    }
+
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& pre) {
+        vector<vector<int>> graph(n);
+
+        for (auto &p : pre){
+            graph[p[1]].push_back(p[0]);
         }
-        return vector<int>();
+
+        list<int> order;
+        vector<int> vis(n, 0);
+        for (int i = 0; i < n; i ++){
+            if (vis[i] == 0){
+                if (dfs(graph, vis, order, i) == false){
+                    return vector<int>();
+                }
+            }
+        }
+
+        vector<int> res(order.begin(), order.end());
+        return res;
     }
 };
