7b7185ec8c4f04aec690966472315635e839ce96
Saisha Verma <saishaverma0512@gmail.com>
2025-11-03 22:58:16 +0530

    Time: 3 ms (73.22%) | Memory: 18.2 MB (66.62%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,34 +1,33 @@
+// BFS Approach - Kahn's Algorithm : 
 class Solution {
 public:
     vector<int> findOrder(int n, vector<vector<int>>& pre) {
-        int m = pre.size();
-
-        // Setting up the indegree & the graph vector : 
         vector<vector<int>> graph(n);
-        vector<int> indegree(n);
+        vector<int> indegree(n, 0);
+
+        // Adjacency List for the graph : 
         for (auto &p : pre){
             graph[p[1]].push_back(p[0]);
             indegree[p[0]] ++;
         }
 
+        // Checking for nodes with indegree = 0 & push them into a queue : 
         queue<int> q;
-        // Searching for the nodes with indegree = 0 : 
         for (int i = 0; i < n; i ++){
-            if (indegree[i] == 0){
+            if(indegree[i] == 0){
                 q.push(i);
             }
         }
-
-        vector<int> order;
-        // Traversing throughout the graph, starting from the nodes with indegree = 0 : 
+        
+        // BFS Kahn's Algo :
+        vector<int> order; 
         while (!q.empty()){
-            auto curr = q.front();
+            int node = q.front();
             q.pop();
-            order.push_back(curr);
+            order.push_back(node);
 
-            for (int nbr : graph[curr]){
+            for (auto nbr : graph[node]){
                 indegree[nbr] --;
-
                 if (indegree[nbr] == 0){
                     q.push(nbr);
                 }
