820663e28a811676e843c26d6b652a0a3b08bda5
Saisha Verma <saishaverma0512@gmail.com>
2025-11-10 03:00:05 +0530

    Time: 4 ms (83.97%) | Memory: 35.2 MB (58.68%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,41 +1,43 @@
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        const int mod = 1e9 + 7;
-        // Adjacency List : 
+        int mod = 1e9 + 7;
+        // Adjacency List of the graph : 
         vector<vector<pair<int, int>>> graph(n);
-        for (auto &way : roads){
-            int u = way[0], v = way[1], w = way[2];
-            graph[u].push_back({v, w}); // {nbr, wt}
-            graph[v].push_back({u, w});
+        for (auto &path : roads){
+            int u = path[0], v = path[1], t = path[2];
+            graph[u].push_back({v, t});
+            graph[v].push_back({u, t});
         }
 
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq; // {Dist, Node} - Min Heap
-        vector<int> ways(n, 0); // To store the no of ways to reach that node
-        vector<long long> dist(n, LLONG_MAX); // To store the minimum distance
-
-        dist[0] = 0;
+        vector<long long> ways(n, 0); // To store the no of ways for each node
+        vector<long long> time(n, LLONG_MAX); // To store minimum dist for each node
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq; // Min - Heap
+        time[0] = 0;
         ways[0] = 1;
-        pq.push({0, 0}); // Pushing the source node
-
+        pq.push({0, 0});
+        // Iterating over the priority queue : 
         while (!pq.empty()){
-            auto [nodeDist, node] = pq.top();
+            auto [nodeTime, node] = pq.top();
             pq.pop();
 
-            if (nodeDist > dist[node]){
+            if (nodeTime > time[node]){ // If the current nodeTime is greater than the minimum time encountered till now, then we just ignore it
                 continue;
             }
 
-            // Iterating over the neighbours : 
+            // Iterating over the neighbours of the current node : 
             for (auto &[nbr, wt] : graph[node]){
-                // Updating the dist(time taken to travel till that intersection) iff the following is satisfied : 
-                if (dist[nbr] > nodeDist + wt){
-                    dist[nbr] = nodeDist + wt;
-                    ways[nbr] = ways[node];
-                    pq.push({dist[nbr], nbr});
-                }
-                else if (dist[nbr] == nodeDist + wt){
+                long long newTime = nodeTime + wt;
+                // If the current time is equal to the minimum time encountered till now : 
+                if (newTime == time[nbr]){
                     ways[nbr] = (ways[nbr] + ways[node]) % mod;
+                }
+
+                // If we find a new minimum time to reach this nbr : 
+                if (newTime < time[nbr]){
+                    time[nbr] = newTime;
+                    ways[nbr] = ways[node];
+                    pq.push({time[nbr], nbr});
                 }
             }
         }
