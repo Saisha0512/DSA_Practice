69b3324c449fd239f736a8ce947bc5e5cde2ddd7
Saisha Verma <saishaverma0512@gmail.com>
2025-12-07 03:49:08 +0530

    Time: 1 ms (92.84%) | Memory: 22.2 MB (90.03%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -1,62 +1,64 @@
 class Solution {
-private : 
-    vector<pair<int, int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-    void dfs(int x, int y, vector<vector<int>> &height, vector<vector<bool>> &vis){
-        int n = height.size(), m = height[0].size();
+    vector<pair<int, int>> dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+    void dfs(vector<vector<int>> &heights, vector<vector<bool>> &vis, int x, int y){
+        int n = heights.size(), m = heights[0].size();
+        // Marking the current node as visited : 
         vis[x][y] = true;
 
         for (auto [dx, dy] : dirs){
             int i = x + dx, j = y + dy;
-            if (i < 0 || i >= n || j < 0 || j >= m || vis[i][j] || height[i][j] < height[x][y]){
+
+            if (i < 0 || j < 0 || i >= n || j >= m || heights[i][j] < heights[x][y] || vis[i][j]){
                 continue;
             }
 
-            dfs(i, j, height, vis);
+            dfs(heights, vis, i, j);
         }
     }
 
-public :
+public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         int n = heights.size(), m = heights[0].size();
         vector<vector<bool>> pacific(n, vector<bool>(m, false));
-        vector<vector<bool>> atlantic(n, vector<bool>(m, false));    
+        vector<vector<bool>> atlantic(n, vector<bool>(m, false));
 
-        // Traversing all the boundaries :  
+        // Traversing the boundaries :
         // Traversing the columns : 
         for (int i = 0; i < n; i ++){
-            // First Column - Pacific Ocean : 
+            // First column : 
             if (!pacific[i][0]){
-                dfs(i, 0, heights, pacific);
+                dfs(heights, pacific, i, 0);
             }
 
-            // Last Column - Atlantic Ocean : 
+            // Last Column : 
             if (!atlantic[i][m - 1]){
-                dfs(i, m - 1, heights, atlantic);
+                dfs(heights, atlantic, i, m - 1);
             }
         }
-
         // Traversing the rows : 
         for (int i = 0; i < m; i ++){
-            // First Row - Pacific Ocean : 
+            // First Row : 
             if (!pacific[0][i]){
-                dfs(0, i, heights, pacific);
+                dfs(heights, pacific, 0, i);
             }
-
-            // Last Row - Atlantic Ocean : 
+            // Last Row : 
             if (!atlantic[n - 1][i]){
-                dfs(n - 1, i, heights, atlantic);
+                dfs(heights, atlantic, n - 1, i);
             }
         }
 
-        // Adding into the result vector : 
+
+        // Iterating to check all the cells who can send water to both pacific & atlantic ocean : 
         vector<vector<int>> res;
         for (int i = 0; i < n; i ++){
             for (int j = 0; j < m; j ++){
-                if (pacific[i][j] && atlantic[i][j]){
+                if (atlantic[i][j] && pacific[i][j]){
                     res.push_back({i, j});
                 }
             }
         }
+
         return res;
     }
 };
