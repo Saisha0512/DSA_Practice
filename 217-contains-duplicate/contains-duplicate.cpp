9fcf6a844f99d582cd7de22b1fe5d9e46a7e3763
Saisha Verma <saishaverma0512@gmail.com>
2025-12-06 21:50:28 +0530

    Time: 59 ms (25.88%) | Memory: 90.7 MB (70.24%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         int n = nums.size();
-        set<int> s;
+        unordered_set<int> s;
 
         for (int num : nums){
             // If the current element already appears in the set : 
