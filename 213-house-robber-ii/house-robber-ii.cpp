759d84491cba12679662996e6929392e0a1ffc84
Saisha Verma <saishaverma0512@gmail.com>
2025-12-09 00:38:48 +0530

    Time: 4 ms (1.53%) | Memory: 10.8 MB (37.97%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,42 +1,46 @@
 class Solution {
-    int robHouses(vector<int> &nums, vector<int> &dp, int s, int e){
-        // Base Case :
+    int robHouse(vector<int> &nums, vector<int> &dp, int s, int e){
+        // Base Case : 
         if (s > e){
             return 0;
         }
 
-        // Check DP : 
+        // Checking the DP : 
         if (dp[s] != -1){
             return dp[s];
         }
 
         // Recursive Case : 
-        // Case 1 : If the current house is skipped & the next house is robbed : 
-        int skip = robHouses(nums, dp, s + 1, e);
-
-        // Case 2 : If the current house is robbed & the next house is skipped : 
-        int current = nums[s] + robHouses(nums, dp, s + 2, e);
-
-        return dp[s] = max(skip, current);
+        // Case 1 : Leaving the current house & robbing the next house : 
+        int skip = robHouse(nums, dp, s + 1, e);
+        // Case 2 : Robbing the current house & leaving the next house : 
+        int theft = robHouse(nums, dp, s + 2, e) + nums[s];
+        
+        return dp[s] = max(skip, theft);
     }
 
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        
         // Special Cases : 
-        if (n == 1){
+        if (n == 0){
+            return 0;
+        }
+        else if (n == 1){
             return nums[0];
         }
-        if (n == 2){
+        else if (n == 2){
             return max(nums[0], nums[1]);
         }
 
-        vector<int> dp1(n, -1), dp2(n, -1);
-        // Case 1 : Start robbing from the 0th indexed house till (n - 2)th house : 
-        int op1 = robHouses(nums, dp1, 0, n - 2);
-        // Case 2 : Start robbing from the 1th - indexed house till (n - 1)th house : 
-        int op2 = robHouses(nums, dp2, 1, n - 1);
+        // Initializing the dp vectors : 
+        // Case 1 : We start from the 0th index till the n - 2 th index
+        vector<int> dp1(n, -1);
+        int op1 = robHouse(nums, dp1, 0, n - 2);
+
+        // Case 2 : We start from the 1st index till the n - 1 th index 
+        vector<int> dp2(n, -1);
+        int op2 = robHouse(nums, dp2, 1, n - 1);
 
         return max(op1, op2);
     }
