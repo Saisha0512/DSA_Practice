5ccb71de6b3ad4d1ad6a7d9377578ef81a491e40
Saisha Verma <saishaverma0512@gmail.com>
2026-01-02 01:38:12 +0530

    Time: 0 ms (100.00%) | Memory: 89.3 MB (81.85%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.cpp
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.cpp
@@ -2,19 +2,14 @@ class Solution {
 public:
     int waysToSplitArray(vector<int>& nums) {
         int n = nums.size();
-
-        vector<long long> left(n, 0), right(n, 0);
-        for (int i = 0; i < n; i ++){
-            // Left prefix : 
-            left[i] = nums[i] + ((i > 0)? left[i - 1] : 0);
-
-            // Right prefix :
-            right[n - 1 - i] = nums[n - 1 - i] + ((i > 0)? right[n - i] : 0);
-        }
+        long long total = 0;
+        for (int num : nums)    total += num;
 
         int cnt = 0;
+        long long pre = 0;
         for (int i = 0; i < n - 1; i ++){
-            if (left[i] >= right[i + 1]){
+            pre += nums[i];
+            if (pre >= total - pre){
                 cnt ++;
             }
         }
