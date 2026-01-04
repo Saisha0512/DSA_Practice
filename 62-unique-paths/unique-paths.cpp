470a8efe00324e7818aea5262a361b46138f3da4
Saisha Verma <saishaverma0512@gmail.com>
2026-01-05 02:37:31 +0530

    Time: 2 ms (19.75%) | Memory: 9.5 MB (27.56%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int uniquePaths(int n, int m) {
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+
+        for (int i = 0; i < n; i ++){
+            for (int j = 0; j < m; j ++){
+                // Base Case : 
+                if (i == 0 && j == 0){
+                    dp[i][j] = 1;
+                }
+                else {
+                    dp[i][j] = ((i > 0)? dp[i - 1][j] : 0) + ((j > 0)? dp[i][j - 1] : 0);
+                }
+            }
+        }
+
+        return dp[n - 1][m - 1];
+    }
+};
