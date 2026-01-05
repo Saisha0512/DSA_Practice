a3c9874646e87188b19a06cd8b25b69b30b58b97
Saisha Verma <saishaverma0512@gmail.com>
2026-01-06 04:09:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (61.38%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,29 +1,21 @@
 class Solution {
-    int recRobbery(vector<int> &nums, int i, vector<int> &dp){
-        // Base Case : 
-        if (i >= nums.size()){
-            return 0;
-        }
-
-        // Check DP : 
-        if (dp[i] != -1){
-            return dp[i];
-        }
-
-        // Recursive Case : 
-        // Case 1 : Robbing the current house & then moving to (i + 2)th house : 
-        int ans1 = nums[i] + recRobbery(nums, i + 2, dp);
-
-        // Case 2 : Leaving the current house & robbing the (i + 1)th house : 
-        int ans2 = recRobbery(nums, i + 1, dp);
-
-        return dp[i] = max(ans1, ans2);
-    }
-
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, -1);
-        return recRobbery(nums, 0, dp);
+        
+        // Base Cases : 
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+
+        vector<int> dp(n, 0);
+        // dp[i] = Max money that can be robbed from houses at the index : 0...i 
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for (int idx = 2; idx < n; idx ++){
+            dp[idx] = max(dp[idx - 1], dp[idx - 2] + nums[idx]);
+        }
+
+        return dp[n - 1];
     }
 };
