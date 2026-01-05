6cc70d541b8f0b2de3592ffd46a7e09640c9d529
Saisha Verma <saishaverma0512@gmail.com>
2026-01-06 03:04:53 +0530

    Time: 155 ms (30.90%) | Memory: 17.3 MB (68.19%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,43 +1,44 @@
-// TOP - DOWN APPROACH : 
+// BOTTOM - UP APPROACH : 
 class Solution {
-    int checkSum(vector<int> &nums, int idx, int target, vector<vector<int>> &dp){
-        // Base Cases : 
-        if (target == 0){
-            return true;
-        }
-        if (idx == 0){
-            return (nums[0] == target);
-        }
-
-        // Check DP : 
-        if (dp[idx][target] != -1){
-            return dp[idx][target];
-        }
-
-        // Recursive Case : 
-        // Case 1 : Not taking the current element into the subsequence : 
-        bool ans1 = checkSum(nums, idx - 1, target, dp);
-
-        // Case 2 : Taking the current element into the subsequence : 
-        bool ans2 = false;
-        if (target >= nums[idx]){
-            ans2 = checkSum(nums, idx - 1, target - nums[idx], dp);
-        }
-
-        return dp[idx][target] = (ans1 || ans2);
-    }
-
 public:
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
-
         int sum = 0;
         for (auto num : nums)   sum += num;
+
         if (sum % 2 != 0){
             return false;
         }
 
-        vector<vector<int>> dp(n, vector<int>(sum / 2 + 1, -1));
-        return checkSum(nums, n - 1, sum / 2, dp);
+        int target = sum / 2;
+        vector<vector<bool>> dp(n, vector<bool>(target + 1, false));
+
+        // Base Cases : 
+        // For all values of idx, if target == 0, then the ans will be true : 
+        for (int i = 0; i < n; i ++){
+            dp[i][0] = true;
+        }
+        // If idx == 0, then if target[0] = nums[0], the ans will be true : 
+        if (nums[0] <= target){
+            dp[0][nums[0]] = true;
+        }
+
+        // Bottom up loop : 
+        for (int i = 1; i < n; i ++){
+            for (int j = 1; j <= target; j ++){
+                // Case 1 : If we dont take the current element, nums[i] into our subsequence : 
+                bool notTake = dp[i - 1][j];
+                
+                // Case 2 : If we take the current element (nums[i]) into our subsequence : 
+                bool take = false;
+                if (j >= nums[i]){
+                    take = dp[i - 1][j - nums[i]];
+                }
+
+                dp[i][j] = notTake || take;
+            }
+        }
+
+        return dp[n - 1][target];
     }
 };
