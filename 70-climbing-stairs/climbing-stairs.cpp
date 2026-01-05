5bc5859395827e947be6c1e3d5d4fc8452f2694b
Saisha Verma <saishaverma0512@gmail.com>
2026-01-06 03:58:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (33.56%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,21 +1,13 @@
 class Solution {
-    int helper(int n, vector<int> &dp){
-        // Base Case : 
-        if (n == 1 || n == 2){
-            return n;
-        }
-
-        // Recursive Case : 
-        if (dp[n] != 0){
-            return dp[n];
-        }
-        int ans = helper(n - 1, dp) + helper(n - 2, dp);
-        return dp[n] = ans;
-    }
-
 public:
     int climbStairs(int n) {
-        vector<int> dp(50, 0);
-        return helper(n, dp);
+        vector<int> dp(n + 1, 0);
+        dp[0] = dp[1] = 1;
+
+        for (int step = 2; step <= n; step ++){
+            dp[step] = ((step - 1 >= 0)? dp[step - 1] : 0) + ((step - 2 >= 0)? dp[step - 2] : 0);
+        }
+
+        return dp[n];
     }
 };
