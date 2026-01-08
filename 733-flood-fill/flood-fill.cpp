1e005669122c3bf3ddc2d41f908182c83ff90a1f
Saisha Verma <saishaverma0512@gmail.com>
2026-01-09 04:57:00 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (31.05%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -2,24 +2,29 @@ class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n = image.size(), m = image[0].size();
+
         int org = image[sr][sc];
         if (org == color){
             return image;
         }
-        queue<pair<int, int>> q;
-        q.push({sr, sc});
+        image[sr][sc] = color;
 
         vector<pair<int, int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        queue<pair<int, int>> q;
+        q.push({sr, sc});
         while (!q.empty()){
             auto [x, y] = q.front();
             q.pop();
-            image[x][y] = color;
 
-            for (auto [dx, dy] : dirs){
+            for (auto &[dx, dy] : dirs){
                 int i = x + dx, j = y + dy;
-                if (i >= 0 && j >= 0 && i < n && j < m && image[i][j] == org){
-                    q.push({i, j});
+
+                if (i < 0 || i >= n || j < 0 || j >= m || image[i][j] != org){
+                    continue;
                 }
+
+                image[i][j] = color;
+                q.push({i, j});
             }
         }
 
