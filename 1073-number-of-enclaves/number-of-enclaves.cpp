93ee87ec94729ef243bceb1b91ec68b2897dfaa6
Saisha Verma <saishaverma0512@gmail.com>
2026-01-10 05:25:52 +0530

    Time: 10 ms (47.27%) | Memory: 34.8 MB (47.91%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,55 +1,40 @@
 class Solution {
+    vector<pair<int, int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    void dfs(vector<vector<int>> &grid, int x, int y, vector<vector<bool>> &vis){
+        // Base Case : 
+        if (x < 0 || y < 0 || x >= grid.size() || y >= grid[0].size() || grid[x][y] == 0 || vis[x][y]){
+            return;
+        }
+
+        // Visiting the current cell : 
+        vis[x][y] = true;
+
+        // Recursive Case : 
+        for (auto [dx, dy] : dirs){
+            dfs(grid, x + dx, y + dy, vis);
+        }
+    }
+
 public:
     int numEnclaves(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
 
         vector<vector<bool>> vis(n, vector<bool>(m, false));
-        queue<pair<int, int>> q;
-        // Traversing all the boundaries : 
-        for (int i = 0; i < n; i ++){
-            // First Column : 
-            if (grid[i][0] == 1){
-                q.push({i, 0});
-                vis[i][0] = true;
-            }
-            // Last Column : 
-            if (grid[i][m - 1] == 1){
-                q.push({i, m - 1});
-                vis[i][m - 1] = true;
-            }
-        }
+        // Iterating over the boundary rows : 
         for (int i = 0; i < m; i ++){
-            // First Row : 
-            if (grid[0][i] == 1){
-                q.push({0, i});
-                vis[0][i] = true;
-            }
-            // Last Row : 
-            if (grid[n - 1][i] == 1){
-                q.push({n - 1, i});
-                vis[n - 1][i] = true;
-            }
+            dfs(grid, 0, i, vis);
+            dfs(grid, n - 1, i, vis);
         }
-
-        vector<pair<int, int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        while (!q.empty()){
-            auto [x, y] = q.front();
-            q.pop();
-
-            for (auto [dx, dy] : dirs){
-                int i = x + dx, j = y + dy;
-                
-                if (i >= 0 && j >= 0 && i < n && j < m && !vis[i][j] && grid[i][j] == 1){
-                    q.push({i, j});
-                    vis[i][j] = true;
-                }
-            }
+        // Iterating over the boundary columns : 
+        for (int i = 0; i < n; i ++){
+            dfs(grid, i, 0, vis);
+            dfs(grid, i, m - 1, vis);
         }
 
         int cnt = 0;
         for (int i = 0; i < n; i ++){
             for (int j = 0; j < m; j ++){
-                if (!vis[i][j] && grid[i][j] == 1){
+                if (grid[i][j] == 1 && !vis[i][j]){
                     cnt ++;
                 }
             }
