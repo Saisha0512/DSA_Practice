7c699bb5bc8a42152e3b9881b9a084fbf8c39e70
Saisha Verma <saishaverma0512@gmail.com>
2026-01-09 20:58:36 +0530

    Time: 18 ms (59.89%) | Memory: 34.6 MB (92.55%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -2,27 +2,32 @@ class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size(), m = mat[0].size();
-        vector<vector<int>> res(n, vector<int>(m, -1));
-        queue<pair<int, int>> q;
 
+        vector<vector<int>> res(n, vector<int>(m, INT_MAX));
+        vector<pair<int, int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        queue<pair<int, int>> q;
         for (int i = 0; i < n; i ++){
             for (int j = 0; j < m; j ++){
                 if (mat[i][j] == 0){
-                    res[i][j] = 0;
                     q.push({i, j});
+                    res[i][j] = 0;
                 }
             }
         }
 
-        vector<pair<int, int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
         while (!q.empty()){
             auto [x, y] = q.front();
             q.pop();
+            int currdist = res[x][y];
 
             for (auto [dx, dy] : dirs){
                 int i = x + dx, j = y + dy;
-                if (i >= 0 && j >= 0 && i < n && j < m && res[i][j] == -1){
-                    res[i][j] = res[x][y] + 1;
+                if (i < 0 || i >= n || j < 0 || j >= m || mat[i][j] == 0){
+                    continue;
+                }
+
+                if (res[i][j] > currdist + 1){
+                    res[i][j] = currdist + 1;
                     q.push({i, j});
                 }
             }
