d99cbb2d11b88b996fd10e4be5aae98a6ebe8b73
Saisha Verma <saishaverma0512@gmail.com>
2026-01-10 00:13:23 +0530

    Time: 3 ms (24.67%) | Memory: 14.9 MB (28.69%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,55 +1,52 @@
 class Solution {
-private : 
-vector<pair<int, int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-
-void dfs(vector<vector<char>> &board, vector<vector<bool>> &vis, int x, int y){
-    int n = board.size(), m = board[0].size();
-    for (auto [dx, dy] : dirs){
-        int i = x + dx, j = y + dy;
-        if (i < 0 || i >= n || j < 0 || j >= m || vis[i][j] || board[i][j] == 'X'){
-            continue;
-        }
-
-        vis[i][j] = true;
-        dfs(board, vis, i, j);
-    }
-}
-
-public :
+public:
     void solve(vector<vector<char>>& board) {
         int n = board.size(), m = board[0].size();
-        vector<vector<bool>> vis(n, vector<bool>(m, false)); // To mark the visited O as true - these cannot be converted
-
-        // TRAVERSING BOUNDARIES : 
-        // Traversing the first & the last columns :  
+        
+        queue<pair<int, int>> q;
+        vector<vector<bool>> vis(n, vector<bool>(m, false));
         for (int i = 0; i < n; i ++){
-            //  First Column : 
-            if (board[i][0] == 'O' && !vis[i][0]){
+            // First row : 
+            if (board[i][0] == 'O'){
                 vis[i][0] = true;
-                dfs(board, vis, i, 0);
-            }
-            // Last Column : 
-            if (board[i][m - 1] == 'O' && !vis[i][m - 1]){
-                vis[i][m - 1] = true;
-                dfs(board, vis, i, m - 1);
-            }
-        }
-
-        // Traversing the first & the last row : 
-        for (int i = 0; i < m; i ++){
-            // First Row : 
-            if (board[0][i] == 'O' && !vis[0][i]){
-                vis[0][i] = true;
-                dfs(board, vis, 0, i);
+                q.push({i, 0});
             }
             // Last Row : 
-            if (board[n - 1][i] == 'O' && !vis[n - 1][i]){
-                vis[n - 1][i] = true;
-                dfs(board, vis, n - 1, i);
+            if (board[i][m - 1] == 'O'){
+                vis[i][m - 1] = true;
+                q.push({i, m - 1});
+            }
+        }
+        for (int j = 0; j < m; j ++){
+            // First row : 
+            if (board[0][j] == 'O'){
+                vis[0][j] = true;
+                q.push({0, j});
+            }
+            // Last Row : 
+            if (board[n - 1][j] == 'O'){
+                vis[n - 1][j] = true;
+                q.push({n - 1, j});
             }
         }
 
-        // Converting the Os into X that are not visited : 
+        // Iterating over all the neighbours - O to mark them that they cannot be converted to X : 
+        vector<pair<int, int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        while (!q.empty()){
+            auto [x, y] = q.front();
+            q.pop();
+
+            for (auto [dx, dy] : dirs){
+                int i = x + dx, j = y + dy;
+
+                if (i >= 0 && j >= 0 && i < n && j < m && board[i][j] == 'O' && !vis[i][j]){
+                    vis[i][j] = true;
+                    q.push({i, j});
+                }
+            }
+        }
+
+        // Checking which Os can be converted to Xs : 
         for (int i = 0; i < n; i ++){
             for (int j = 0; j < m; j ++){
                 if (board[i][j] == 'O' && !vis[i][j]){
