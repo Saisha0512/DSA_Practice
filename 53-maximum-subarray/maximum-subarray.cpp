784177bbac0d6c1a72658811695f1f537cf84247
Saisha Verma <saishaverma0512@gmail.com>
2026-01-10 23:33:38 +0530

    Time: 0 ms (100.00%) | Memory: 71.6 MB (80.14%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
-    // Implementing the Kadane's Algorithm for the maximum sum subarray : 
     int maxSubArray(vector<int>& nums) {
-        int n = nums.size();
         int currsum = 0, maxsum = INT_MIN;
-
-        // Iterating for each element in the array & calculating the prefix sum : 
-        for (int i = 0; i < n; i ++){
-            currsum += nums[i];
-
-            maxsum = max(maxsum, currsum); // Updating the maxsum
-
-            // If the currsum becomes negative, then we make it equal to 0, so as to consider the subarrays possible by the positive elements after the current element : 
+        for (int num : nums){
+            currsum += num;
+            maxsum = max(maxsum, currsum);
+            
             if (currsum < 0){
                 currsum = 0;
             }
