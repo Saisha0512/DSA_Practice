28f09845463381f2572ae5e100c7ad5d879a1401
Saisha Verma <saishaverma0512@gmail.com>
2026-01-11 00:17:50 +0530

    Time: 5 ms (56.86%) | Memory: 19.2 MB (68.07%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,44 +1,41 @@
-// DFS Approach : 
+// BFS APPROACH : 
 class Solution {
-    bool dfs(vector<vector<int>> &graph, vector<int> &vis, int node){
-        // Visiting the current node : 
-        vis[node] = 1;
-
-        // Checking the neighbours : 
-        for (int nbr : graph[node]){
-            if(vis[nbr] == 1){
-                return false; // Cycle Detected
-            }
-            if (vis[nbr] == 0){
-                bool subprob = dfs(graph, vis, nbr);
-                if (subprob == false){
-                    return false; // Cycle Detected
-                }
-            }
-        }
-
-        // Visited the node : 
-        vis[node] = 2;
-        return true;
-    }
-
 public:
     bool canFinish(int n, vector<vector<int>>& pre) {
+        // Constructing the graph : 
+        vector<int> indegree(n, 0);
         vector<vector<int>> graph(n);
-        for (int i = 0; i < pre.size(); i ++){
-            int a = pre[i][0], b = pre[i][1];
-            graph[b].push_back(a);
+        for (auto &p : pre){
+            int a = p[0], b = p[1];
+
+            graph[b].push_back(a); // Directed graph
+            indegree[a] ++;
         }
 
-        vector<int> vis(n, 0);
+        queue<int> q;
+        // Checking for the node with indegree = 0
         for (int i = 0; i < n; i ++){
-            if(vis[i] == 0){
-                if (dfs(graph, vis, i) == false){
-                    return false; // Cycle detected
+            if (indegree[i] == 0){
+                q.push(i);
+            }
+        }
+
+        // Iterating over the queue :
+        int cnt = 0; // To maintain the count of the courses that are completed
+        while (!q.empty()){
+            int curr = q.front();
+            q.pop();
+            cnt ++;
+
+            for (int nbr : graph[curr]){
+                indegree[nbr] --;
+
+                if (indegree[nbr] == 0){
+                    q.push(nbr);
                 }
             }
         }
 
-        return true;
+        return (cnt == n);
     }
 };
