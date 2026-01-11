2fa4a15d57969371a9e5ce7dde0acbdbc5d9a99f
Saisha Verma <saishaverma0512@gmail.com>
2026-01-11 23:16:28 +0530

    Time: 20 ms (65.61%) | Memory: 85.7 MB (31.80%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,20 +1,47 @@
 class Solution {
-public:
-    int largestRectangleArea(vector<int>& heights) {
-        heights.push_back(0);
+    vector<int> pseFunction(vector<int> &heights){
         int n = heights.size();
+        vector<int> pse(n);
         stack<int> s;
-        int maxarea = 0;
 
         for (int i = 0; i < n; i ++){
             while (!s.empty() && heights[s.top()] >= heights[i]){
-                int h = heights[s.top()];
                 s.pop();
-                int w = i - ((s.empty())? -1 : s.top()) - 1;
-                maxarea = max(maxarea, h * w);
             }
 
+            pse[i] = ((s.empty())? -1 : s.top());
             s.push(i);
+        }
+
+        return pse;
+    }
+
+    vector<int> nseFunction(vector<int> &heights){
+        int n = heights.size();
+        vector<int> nse(n);
+        stack<int> s;
+
+        for (int i = n - 1; i >= 0; i --){
+            while (!s.empty() && heights[s.top()] >= heights[i]){
+                s.pop();
+            }
+
+            nse[i] = ((s.empty())? n : s.top());
+            s.push(i);
+        }
+
+        return nse;
+    }
+
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> pse = pseFunction(heights), nse = nseFunction(heights);
+        int maxarea = 0;
+
+        for (int i = 0; i < n; i ++){
+            int currarea = heights[i] * (nse[i] - pse[i] - 1);
+            maxarea = max(maxarea, currarea);
         }
 
         return maxarea;
