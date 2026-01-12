1be919100db1370d33577ef35a3e641195c02b3d
Saisha Verma <saishaverma0512@gmail.com>
2026-01-13 02:32:25 +0530

    Time: 30 ms (67.62%) | Memory: 121.8 MB (63.22%) - LeetSync

--- 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.cpp
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.cpp
@@ -3,14 +3,10 @@ public:
     vector<vector<int>> onesMinusZeros(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
 
-        vector<int> row1(n, 0), row0(n, 0), col1(m, 0), col0(m, 0);
+        vector<int> row1(n, 0), col1(m, 0);
         for (int i = 0; i < n; i ++){
             for (int j = 0; j < m; j ++){
-                if (grid[i][j] == 0){
-                    row0[i] ++;
-                    col0[j] ++;
-                }
-                else {
+                if (grid[i][j] == 1){
                     row1[i] ++;
                     col1[j] ++;
                 }
@@ -20,7 +16,7 @@ public:
         vector<vector<int>> diff(n, vector<int>(m, 0));
         for (int i = 0; i < n; i ++){
             for (int j = 0; j < m; j ++){
-                diff[i][j] = row1[i] + col1[j] - row0[i] - col0[j];
+                diff[i][j] = row1[i] + col1[j] - (m - row1[i]) - (n - col1[j]);
             }
         }
 
