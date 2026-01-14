c7ce78132eb185530c43a27d17aac0530e6b38f4
Saisha Verma <saishaverma0512@gmail.com>
2026-01-15 02:31:32 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (83.44%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,47 +1,43 @@
-// DFS Approach : 
+// BFS APPROACH (Kahn's Algo - Indegree) : 
 class Solution {
-    bool dfs(vector<vector<int>> &graph, vector<int> &vis, list<int> &order, int node){
-        // Visiting the current node : 
-        vis[node] = 1;
-
-        // Iterating over all the neighbours : 
-        for (auto nbr : graph[node]){
-            if (vis[nbr] == 1){ // nbr is in the current path already - Cycle Detected
-                return false;
-            }
-            if (vis[nbr] == 0){
-                if (dfs(graph, vis, order, nbr) == false){
-                    return false;
-                }
-            }
-        }
-
-        // After visiting & adding all the neighbours of the current node to the order, 
-        // we add the current node to the order list from the front : 
-        vis[node] = 2;
-        order.push_front(node);
-        return true;
-    }
-
 public:
     vector<int> findOrder(int n, vector<vector<int>>& pre) {
+        // Constructing the graph : 
+        vector<int> indegree(n, 0);
         vector<vector<int>> graph(n);
-
         for (auto &p : pre){
-            graph[p[1]].push_back(p[0]);
+            int a = p[0], b = p[1];
+            graph[b].push_back(a);
+            indegree[a] ++;
         }
 
-        list<int> order;
-        vector<int> vis(n, 0);
+        vector<int> order;
+        queue<int> q;
+        // Checking for nodes with indegree = 0 : 
         for (int i = 0; i < n; i ++){
-            if (vis[i] == 0){
-                if (dfs(graph, vis, order, i) == false){
-                    return vector<int>();
+            if (indegree[i] == 0){
+                q.push(i);
+            }
+        }
+
+        while (!q.empty()){
+            int curr = q.front();
+            q.pop();
+
+            order.push_back(curr);
+            for (auto nbr : graph[curr]){
+                indegree[nbr] --;
+
+                if (indegree[nbr] == 0){
+                    q.push(nbr);
                 }
             }
         }
 
-        vector<int> res(order.begin(), order.end());
-        return res;
+        if (order.size() != n){
+            return {};
+        }
+
+        return order;
     }
 };
