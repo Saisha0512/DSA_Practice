73e7f111106512bba790399ecea5f9c3558e271b
Saisha Verma <saishaverma0512@gmail.com>
2026-01-15 02:42:12 +0530

    Time: 4 ms (54.94%) | Memory: 18.3 MB (56.71%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,43 +1,56 @@
-// BFS APPROACH (Kahn's Algo - Indegree) : 
+// DFS APPROACH : 
 class Solution {
-public:
+    vector<int> order;
+    bool cycle = false;
+
+private : 
+    void dfs(vector<vector<int>> &graph, int node, vector<int> &status){
+        // Visiting the current node : 
+        status[node] = 1;
+
+        // Iterating over all the nbrs : 
+        for (auto nbr : graph[node]){
+            if (status[nbr] == 0){
+                dfs(graph, nbr, status);
+                if (cycle){
+                    return;
+                }
+            }
+            else if (status[nbr] == 1){
+                cycle = true;
+                return;
+            }
+        }
+
+        // Pushing the current node into the order after visiting all the neighbours : 
+        status[node] = 2; // Visited
+        order.push_back(node);
+    }
+
+public :
     vector<int> findOrder(int n, vector<vector<int>>& pre) {
         // Constructing the graph : 
-        vector<int> indegree(n, 0);
         vector<vector<int>> graph(n);
         for (auto &p : pre){
             int a = p[0], b = p[1];
             graph[b].push_back(a);
-            indegree[a] ++;
         }
 
-        vector<int> order;
-        queue<int> q;
-        // Checking for nodes with indegree = 0 : 
+        vector<int> status(n, 0);
         for (int i = 0; i < n; i ++){
-            if (indegree[i] == 0){
-                q.push(i);
-            }
-        }
-
-        while (!q.empty()){
-            int curr = q.front();
-            q.pop();
-
-            order.push_back(curr);
-            for (auto nbr : graph[curr]){
-                indegree[nbr] --;
-
-                if (indegree[nbr] == 0){
-                    q.push(nbr);
+            if (status[i] == 0){
+                dfs(graph, i, status);
+                if (cycle){
+                    return {};
                 }
             }
         }
 
-        if (order.size() != n){
+        if (order.size() != n || cycle){
             return {};
         }
 
+        reverse(order.begin(), order.end());
         return order;
     }
 };
