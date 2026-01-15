675718d545370522c8b4ec77976367a746e07414
Saisha Verma <saishaverma0512@gmail.com>
2026-01-15 23:16:50 +0530

    Time: 3 ms (67.59%) | Memory: 14.7 MB (61.46%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
-        unordered_map<int, int> freq; // {Element, Index}
+        unordered_map<int, int> m;
         for (int i = 0; i < n; i ++){
-            // Checking if target - num is present in the array iterated earlier or not : 
-            if (freq.find(target - nums[i]) != freq.end()){
-                return {freq[target - nums[i]], i};
+            if (m.find(target - nums[i]) != m.end()){
+                return {m[target - nums[i]], i};
             }
 
-            freq[nums[i]] = i;
+            m[nums[i]] = i;
         }
 
         return {-1, -1};
