e300cbb879d5487317521e9518cfd14cf1aa6fdb
Saisha Verma <saishaverma0512@gmail.com>
2026-01-23 19:10:54 +0530

    Time: 481 ms (83.54%) | Memory: 163.1 MB (91.14%) - LeetSync

--- 3772-minimum-pair-removal-to-sort-array-ii/minimum-pair-removal-to-sort-array-ii.cpp
+++ 3772-minimum-pair-removal-to-sort-array-ii/minimum-pair-removal-to-sort-array-ii.cpp
@@ -1,77 +1,86 @@
 class Solution {
 public:
-    using ll = long long;
-
     int minimumPairRemoval(vector<int>& nums) {
         int n = nums.size();
-        if (n <= 1)
+        if (n <= 1){
             return 0;
+        }
 
-        vector<ll> arr(nums.begin(), nums.end());
+        vector<long long> arr(nums.begin(), nums.end());
         vector<bool> removed(n, false);
-        priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<>> pq;
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq; // Min - heap
 
         int sorted = 0;
-        for (int i = 1; i < n; ++i) {
+        for (int i = 1; i < n; i ++){
             pq.push({arr[i - 1] + arr[i], i - 1});
-            if (arr[i] >= arr[i - 1])
-                sorted++;
+            if (arr[i] >= arr[i - 1]){
+                sorted ++;
+            }
         }
+
         if (sorted == n - 1){
             return 0;
         }
 
         int rem = n;
         vector<int> prev(n), next(n);
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; i ++){
             prev[i] = i - 1;
             next[i] = i + 1;
         }
 
-        while (rem > 1) {
+        while (rem > 1){
             auto [sum, left] = pq.top();
             pq.pop();
+
             int right = next[left];
-            if (right >= n || removed[left] || removed[right] ||
-                arr[left] + arr[right] != sum)
-                continue;
+            if (right >= n || removed[left] || removed[right] || arr[left] + arr[right] != sum){
+                continue;      
+            }
 
             int pre = prev[left];
             int nxt = next[right];
-
-            if (arr[left] <= arr[right])
-                sorted--;
-            if (pre != -1 && arr[pre] <= arr[left])
-                sorted--;
-            if (nxt != n && arr[right] <= arr[nxt])
-                sorted--;
-
+            if (arr[left] <= arr[right]){
+                sorted --;
+            }
+            if (pre != -1 && arr[pre] <= arr[left]){
+                sorted --;
+            }
+            if (nxt != n && arr[right] <= arr[nxt]){
+                sorted --;
+            }
+                
             arr[left] += arr[right];
             removed[right] = true;
-            rem--;
+            rem --;
 
-            if (pre == -1) {
+            if (pre == -1){
                 prev[left] = -1;
-            } else {
+            }
+            else {
                 pq.push({arr[pre] + arr[left], pre});
-                if (arr[pre] <= arr[left])
-                    sorted++;
+                if (arr[pre] <= arr[left]){
+                    sorted ++;
+                }
             }
 
-            if (nxt == n) {
+            if (nxt == n){
                 next[left] = n;
-            } else {
+            }
+            else {
                 prev[nxt] = left;
                 next[left] = nxt;
                 pq.push({arr[left] + arr[nxt], left});
-                if (arr[left] <= arr[nxt])
-                    sorted++;
+                if (arr[left] <= arr[nxt]){
+                    sorted ++;
+                }
             }
 
             if (sorted == rem - 1){
                 return n - rem;
             }
         }
+
         return n;
     }
 };
