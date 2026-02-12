89025057c75e97e450f39d948beaef7db2f62120
Saisha Verma <saishaverma0512@gmail.com>
2026-02-13 05:14:00 +0530

    Time: 47 ms (96.29%) | Memory: 25.5 MB (53.32%) - LeetSync

--- 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
@@ -7,7 +7,7 @@ public:
         for (int i = 0; i < n; i ++){
             vector<int> freq(26, 0);
             int maxfreq = 0, dist = 0;
-            
+
             for (int j = i; j < n; j ++){
                 int idx = s[j] - 'a';
                 if (freq[idx] == 0){
@@ -16,8 +16,9 @@ public:
                 freq[idx] ++;
                 maxfreq = max(maxfreq, freq[idx]);
 
-                if (j - i + 1 == dist * maxfreq){
-                    maxlen = max(maxlen, j - i + 1);
+                int currlen = j - i + 1;
+                if (currlen == dist * maxfreq){
+                    maxlen = max(maxlen, currlen);
                 }
             }
         }
