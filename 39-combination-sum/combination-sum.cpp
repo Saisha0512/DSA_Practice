88d47b16178e339d03cd2c07e99d9c19c5fd9058
Saisha Verma <saishaverma0512@gmail.com>
2026-02-15 03:25:23 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (24.89%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    void check(vector<int> &nums, int target, int i, vector<int> &curr, vector<vector<int>> &res){
+        // Base Case : 
+        if (target == 0){
+            res.push_back(curr);
+            return;
+        }
+        if (i == nums.size() || target < 0){
+            return;
+        }
+
+        // Recursive Case : 
+        // Case 1 : Not pick the current element
+        check(nums, target, i + 1, curr, res);
+
+        // Case 2 : Pick the current element
+        curr.push_back(nums[i]);
+        check(nums, target - nums[i], i, curr, res);
+
+        // Backtrack : 
+        curr.pop_back();
+    }
+
+public:
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        vector<int> curr;
+        vector<vector<int>> res;
+        check(nums, target, 0, curr, res);
+
+        return res;
+    }
+};
