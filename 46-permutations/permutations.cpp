423ecbf0bbf3eb35b8563ead30366a39483c7c98
Saisha Verma <saishaverma0512@gmail.com>
2026-02-15 23:48:36 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (50.59%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,12 +1,35 @@
 class Solution {
+    void addPerm(vector<int> &nums, vector<int> &curr, vector<bool> &vis, vector<vector<int>> &res){
+        int n = vis.size();
+        // Base Case : 
+        if (curr.size() == n){
+            res.push_back(curr);
+            return;
+        }
+
+        // Recursive Call : 
+        for (int i = 0; i < n; i ++){
+            if (!vis[i]){
+                curr.push_back(nums[i]);
+                vis[i] = true;
+                // Recursive Call : 
+                addPerm(nums, curr, vis, res);
+
+                // Backtracking : 
+                curr.pop_back();
+                vis[i] = false;
+            }
+        }
+    }
+
 public:
     vector<vector<int>> permute(vector<int>& nums) {
+        int n = nums.size();
         sort(nums.begin(), nums.end());
+        vector<bool> vis(n, false);
+        vector<int> curr;
         vector<vector<int>> res;
-
-        do {
-            res.push_back(nums);
-        } while (next_permutation(nums.begin(), nums.end()));
+        addPerm(nums, curr, vis, res);
 
         return res;
     }
