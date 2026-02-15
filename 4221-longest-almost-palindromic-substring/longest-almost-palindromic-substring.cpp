41d362a699e16a7761f10fa68244d10e930b5c7d
Saisha Verma <saishaverma0512@gmail.com>
2026-02-15 18:07:05 +0530

    Time: 2029 ms (13.96%) | Memory: 10.3 MB (93.70%) - LeetSync

---
+++ 4221-longest-almost-palindromic-substring/longest-almost-palindromic-substring.cpp
@@ -0,0 +1,50 @@
+class Solution {
+    bool isPalindrome(string &s, int l, int r){
+        while (l <= r){
+            if (s[l] == s[r]){
+                l ++;
+                r --;
+            }
+            else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    
+    bool check(string &s, int l, int r){
+        while (l < r){
+            if (s[l] == s[r]){
+                l ++;
+                r --;
+            }
+            else {
+                // Trying to remove the character at l or at r : 
+                return (isPalindrome(s, l + 1, r) || isPalindrome(s, l, r - 1));
+            }
+        }
+
+        return true;
+    }
+    
+public:
+    int almostPalindromic(string s) {
+        int n = s.size();
+
+        int maxlen = 0;
+        for (int i = 0; i < n; i ++){
+            for (int j = i + maxlen; j < n; j ++){
+                if (j - i + 1 <= maxlen){
+                    continue;
+                }
+
+                if (check(s, i, j)){
+                    maxlen = j - i + 1;
+                }
+            }
+        }
+
+        return maxlen;
+    }
+};
