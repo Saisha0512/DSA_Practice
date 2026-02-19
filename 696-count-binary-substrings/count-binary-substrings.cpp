05fff3364ce6b74aa91547020ec073eaf51d12c1
Saisha Verma <saishaverma0512@gmail.com>
2026-02-20 01:08:48 +0530

    Time: 3 ms (67.57%) | Memory: 13 MB (62.16%) - LeetSync

---
+++ 696-count-binary-substrings/count-binary-substrings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+        int n = s.size();
+        int ans = 0, prev = 0, curr = 1;
+
+        for (int i = 1; i < n; i ++){
+            if (s[i] == s[i - 1]){
+                curr ++;
+            }
+            else {
+                ans += min(prev, curr);
+                prev = curr;
+                curr = 1;
+            }
+        }
+
+        ans += min(prev, curr);
+        return ans;
+    }
+};
