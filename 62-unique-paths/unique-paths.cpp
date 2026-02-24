2e226d865965a51f4aef8fa3ed2ede52f918009a
Saisha Verma <saishaverma0512@gmail.com>
2026-02-25 02:22:52 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (5.07%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -3,14 +3,21 @@ public:
     int uniquePaths(int n, int m) {
         vector<vector<int>> dp(n, vector<int>(m, 0));
 
+        // Base Case : 
+        // There is only one way to reach (0, 0) cell : 
+        dp[0][0] = 1;
+
+        // Bottom - up DP Loop : 
         for (int i = 0; i < n; i ++){
             for (int j = 0; j < m; j ++){
-                // Base Case : 
-                if (i == 0 && j == 0){
-                    dp[i][j] = 1;
+                // Case 1 : To reach the current cell, we can come from the upper cell
+                if (i > 0){
+                    dp[i][j] += dp[i - 1][j];
                 }
-                else {
-                    dp[i][j] = ((i > 0)? dp[i - 1][j] : 0) + ((j > 0)? dp[i][j - 1] : 0);
+
+                // Case 2 : To reach the current cell, we can come from the left cell
+                if (j > 0){
+                    dp[i][j] += dp[i][j - 1];
                 }
             }
         }
