5dc8617bd37d2991066c1cfbd1d39d2a83e63102
Saisha Verma <saishaverma0512@gmail.com>
2026-04-19 03:43:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (32.78%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,9 +1,14 @@
+// BOTTOM - UP APPROACH : 
 class Solution {
 public:
     int climbStairs(int n) {
         vector<int> dp(n + 1, 0);
-        dp[0] = dp[1] = 1;
 
+        // base cases
+        dp[0] = 1; // by staying at the ground itself
+        dp[1] = 1; // by climbing one stair
+
+        // bottom up loop
         for (int i = 2; i <= n; i ++){
             dp[i] = dp[i - 1] + dp[i - 2];
         }
