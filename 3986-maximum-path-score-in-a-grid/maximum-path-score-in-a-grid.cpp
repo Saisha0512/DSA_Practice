e29cf13087097f9db1b5a437ad8265f3c6dadf84
Saisha Verma <saishaverma0512@gmail.com>
2026-05-01 02:27:21 +0530

    Time: 999 ms (24.26%) | Memory: 499.3 MB (79.76%) - LeetSync

--- 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.cpp
+++ 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.cpp
@@ -1,53 +1,55 @@
-// TOP - DOWN CODE : 
+// BOTTOM - UP CODE : 
 class Solution {
-    int checkPath(vector<vector<int>> &grid, int i, int j, int k, vector<vector<vector<int>>> &dp){
-        int n = grid.size(), m = grid[0].size();
-
-        // base case :
-        if (i >= n || j >= m){
-            return INT_MIN;
-        }
-
-        int val = grid[i][j];
-        int cost = (val == 0)? 0 : 1;
-
-        // checking the boundary of cost
-        if (cost > k){
-            return INT_MIN;
-        }
-
-        // bottom - right corner reached
-        if (i == n - 1 && j == m - 1){
-            return val;
-        }
-
-        // checking dp
-        if (dp[i][j][k] != -1){
-            return dp[i][j][k];
-        }
-
-        // recursive case
-        // going down
-        int down = checkPath(grid, i + 1, j, k - cost, dp);
-
-        // going right 
-        int right = checkPath(grid, i, j + 1, k - cost, dp);
-
-        int res = max(down, right);
-        if (res == INT_MIN){
-            return dp[i][j][k] = INT_MIN;
-        }
-
-        return dp[i][j][k] = res + val;
-    }
-
 public:
-    int maxPathScore(vector<vector<int>>& grid, int k) {
+    int maxPathScore(vector<vector<int>>& grid, int maxcost) {
         int n = grid.size(), m = grid[0].size();
 
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(k + 1, -1)));
-        int res = checkPath(grid, 0, 0, k, dp);
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(maxcost + 1, INT_MIN)));
+        // dp[i][j][k] = max score to crossing cell (i, j) by the exact cost k
 
-        return (res == INT_MIN)? -1 : res;
+        // dp initialization
+        int cost = (grid[0][0] == 0)? 0 : 1;
+        if (cost <= maxcost){
+            dp[0][0][cost] = grid[0][0];
+        }
+
+        // bottom up loop
+        for (int i = 0; i < n; i ++){
+            for (int j = 0; j < m; j ++){
+                // base case
+                if (i == 0 && j == 0){
+                    continue; // already handled in intialization
+                }
+
+                int currval = grid[i][j];
+                int currcost = (currval == 0)? 0 : 1;
+                for (int k = 0; k <= maxcost; k ++){
+                    // coming from up
+                    if (i - 1 >= 0 && dp[i - 1][j][k] != INT_MIN){
+                        int newcost = currcost + k;
+                        if (newcost <= maxcost){
+                            dp[i][j][newcost] = max(dp[i][j][newcost], dp[i - 1][j][k] + currval);
+                        }
+                    }
+
+                    // coming from left
+                    if (j - 1 >= 0 && dp[i][j - 1][k] != INT_MIN){
+                        int newcost = currcost + k;
+                        if (newcost <= maxcost){
+                            dp[i][j][newcost] = max(dp[i][j][newcost], dp[i][j - 1][k] + currval);
+                        }
+                    }
+                }
+                
+            }
+        }
+
+        // final answer 
+        int ans = INT_MIN;
+        for (int k = 0; k <= maxcost; k ++){
+            ans = max(ans, dp[n - 1][m - 1][k]);
+        }
+
+        return (ans == INT_MIN)? -1 : ans;
     }
 };
