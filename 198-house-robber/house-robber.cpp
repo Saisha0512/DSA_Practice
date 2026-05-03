88e168f0564a8b58251e2f5bc76455e17b30ba5f
Saisha Verma <saishaverma0512@gmail.com>
2026-05-03 22:34:54 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (20.55%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,32 +1,36 @@
+// TOP - DOWN APPROACH :
 class Solution {
+    vector<int> dp;
+
+    int rob(vector<int> &nums, int i){
+        // base case
+        if (i == 0){
+            return nums[i];
+        }
+        if (i == 1){ // you can either rob 0th or 1st house
+            return max(nums[0], nums[1]);
+        }
+
+        // check dp
+        if (dp[i] != -1){
+            return dp[i];
+        }
+
+        // recursive case
+        // case 1 : skip the current house
+        int op1 = rob(nums, i - 1);
+
+        // case 2 : robbing the current house & going for the (i - 2)th house
+        int op2 = nums[i] + rob(nums, i - 2);
+
+        return dp[i] = max(op1, op2);
+    }
+
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        
-        // Base Cases : 
-        if (n == 0) return 0;
-        if (n == 1) return nums[0];
+        dp.resize(n, -1);
 
-        vector<int> dp(n, 0);
-        // dp[i] = Max money that can be robbed from houses at the index : 0...i 
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
-
-        for (int idx = 2; idx < n; idx ++){
-            int amt = INT_MIN;
-            // Case 1 : We dont rob the current house, because the previous house was already robbed 
-            if (idx - 1 >= 0){
-                amt = max(amt, dp[idx - 1]);
-            }
-
-            // Case 2 : We rob the current house, because the (i - 2)th house was robbed 
-            if (idx - 2 >= 0){
-                amt = max(amt, dp[idx - 2] + nums[idx]);
-            }
-
-            dp[idx] = amt;
-        }
-
-        return dp[n - 1];
+        return rob(nums, n - 1);
     }
 };
