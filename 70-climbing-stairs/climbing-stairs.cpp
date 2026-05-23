64a63426b2014070f9660136bbcb3f1d4968bc8c
Saisha Verma <saishaverma0512@gmail.com>
2026-05-23 20:30:11 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (16.90%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,24 +1,25 @@
 // TOP - DOWN APPROACH : 
-vector<int> dp(46, -1);
 class Solution {
+    vector<int> dp;
     int climb(int n){
         // base case
         if (n == 0 || n == 1){
             return 1;
         }
 
-        // check dp
+        // check dp 
         if (dp[n] != -1){
             return dp[n];
         }
 
         // recursive case
-        int ans = climb(n - 1) + climb(n - 2);
-        return dp[n] = ans;
+        dp[n] = climb(n - 1) + climb(n - 2);
+        return dp[n];
     }
 
 public:
     int climbStairs(int n) {
+        dp.resize(n + 1, -1);
         return climb(n);
     }
 };
