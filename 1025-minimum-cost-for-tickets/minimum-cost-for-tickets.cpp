407c05b9c5ff74855e725bd94cc9c9d22c2f1556
Saisha Verma <saishaverma0512@gmail.com>
2026-05-25 12:58:37 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (69.19%) - LeetSync

---
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -0,0 +1,50 @@
+// TOP - DOWN APPROACH : 
+class Solution {
+    vector<int> dp;
+    // dp[i] = minimum cost required to cover first n travel days
+
+    int buy(vector<int> &days, vector<int> &costs, int n){
+        // base case
+        if (n == 0){
+            return 0;
+        }
+
+        // check dp
+        if (dp[n] != -1){
+            return dp[n];
+        }
+
+        // recursive case
+        int tempans = INT_MAX;
+        // trying to buy a ticket such that its validity ends on (n - 1)th day
+        for (int i = 0; i < costs.size(); i ++){
+            int currdays;
+            if (i == 0){
+                currdays = 1;
+            }
+            else if (i == 1){
+                currdays = 7;
+            }
+            else {
+                currdays = 30;
+            }
+
+            int j = n - 2;
+            while (j >= 0 && days[j] >= days[n - 1] - currdays + 1){
+                j --;
+            }
+
+            tempans = min(tempans, costs[i] + buy(days, costs, j + 1));
+        }
+
+        return dp[n] = tempans;
+    }
+
+public:
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        int n = days.size();
+        dp.resize(n + 1, -1);
+
+        return buy(days, costs, n);
+    }
+};
