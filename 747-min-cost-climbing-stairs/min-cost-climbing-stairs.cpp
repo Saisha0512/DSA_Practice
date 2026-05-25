a602ce6ac5b5f538bd812163cab3e4a258875fce
Saisha Verma <saishaverma0512@gmail.com>
2026-05-25 06:37:07 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (27.56%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,31 +1,24 @@
-// TOP - DOWN APPROACH : 
+// BOTTOM - UP LOOP : 
 class Solution {
-    vector<int> dp;
-    // dp[i] = cost to climb reach the ith floor
-
-    int climb(vector<int> &cost, int i){
-        // base case
-        if (i == 0 || i == 1){
-            return 0;
-        }
-
-        // check dp
-        if (dp[i] != -1){
-            return dp[i];
-        }
-
-        // recursive case
-        int op1 = climb(cost, i - 1) + cost[i - 1];
-        int op2 = climb(cost, i - 2) + cost[i - 2];
-
-        return dp[i] = min(op1, op2);
-    }
-
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        dp.resize(n + 1, -1);
 
-        return climb(cost, n);
+        // dp initialization
+        vector<int> dp(n + 1, -1);
+        // dp[i] = cost to reach the ith floor
+        dp[0] = dp[1] = 0;
+
+        for (int i = 2; i <= n; i ++){
+            // climb from 1 floor down
+            int op1 = dp[i - 1] + cost[i - 1];
+            // climb from 2 floors down
+            int op2 = dp[i - 2] + cost[i - 2];
+
+            // taking the minimum of each of these paths
+            dp[i] = min(op1, op2);
+        }
+
+        return dp[n];
     }
 };
