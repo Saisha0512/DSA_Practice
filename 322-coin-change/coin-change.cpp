9711f6de6b731f4fa4db352fc903eac2ee0c6b97
Saisha Verma <saishaverma0512@gmail.com>
2026-05-25 09:37:54 +0530

    Time: 39 ms (41.96%) | Memory: 19 MB (51.49%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,21 +1,39 @@
-// BOTTOM - UP APPROACH : 
+// TOP - DOWN APPROACH : 
 class Solution {
-public:
-    int coinChange(vector<int>& coins, int amt) {
-        vector<int> dp(amt + 1, amt + 1);
+    vector<int> dp;
+    // dp[i] = min no of coins required to make the sum = i
 
-        // Base Case : 
-        dp[0] = 0;
+    int change(vector<int> &coins, int amt){
+        // base case
+        if (amt == 0){
+            return 0;
+        }
 
-        // Iterate through all the amounts possible : 
-        for (int i = 1; i <= amt; i ++){
-            for (int coin : coins){
-                if (i >= coin){
-                    dp[i] = min(dp[i], 1 + dp[i - coin]);
+        // check dp
+        if (dp[amt] != -1){
+            return dp[amt];
+        }
+
+        // recursive case
+        int tempans = INT_MAX;
+        // trying to form the amount from all the possible coin denominations
+        for (int i = 0; i < coins.size(); i ++){
+            if (coins[i] <= amt){
+                // using the ith coint
+                int currans = change(coins, amt - coins[i]);
+                if (currans != INT_MAX){
+                    tempans = min(tempans, currans + 1); // taking the minimum possible answer from all these steps
                 }
             }
         }
 
-        return ((dp[amt] > amt)? -1 : dp[amt]);
+        return dp[amt] = tempans;
+    }
+
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        dp.resize(amount + 1, -1);
+        int res = change(coins, amount);
+        return (res == INT_MAX)? -1 : res;
     }
 };
