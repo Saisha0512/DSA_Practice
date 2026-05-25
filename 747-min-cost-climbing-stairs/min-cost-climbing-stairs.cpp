3851d6905fe400a0ad4534c77f8ef005cb66ecf3
Saisha Verma <saishaverma0512@gmail.com>
2026-05-25 06:34:34 +0530

    Time: 4 ms (4.66%) | Memory: 17.7 MB (48.83%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,35 +1,31 @@
-// TOP - DOWN APPROACH :
+// TOP - DOWN APPROACH : 
 class Solution {
     vector<int> dp;
-    
+    // dp[i] = cost to climb reach the ith floor
+
     int climb(vector<int> &cost, int i){
         // base case
         if (i == 0 || i == 1){
-            return cost[i];
+            return 0;
         }
 
-        // check dp 
+        // check dp
         if (dp[i] != -1){
             return dp[i];
         }
 
         // recursive case
-        // case 1 : coming from 1 step down
-        int op1 = climb(cost, i - 1);
+        int op1 = climb(cost, i - 1) + cost[i - 1];
+        int op2 = climb(cost, i - 2) + cost[i - 2];
 
-        // case 2 : coming from 2 steps down
-        int op2 = climb(cost, i - 2);
-
-        // result will be the minimum cost + cost of climbing the current step
-        return dp[i] = cost[i] + min(op1, op2);
+        return dp[i] = min(op1, op2);
     }
 
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        dp.resize(n, -1);
+        dp.resize(n + 1, -1);
 
-        // we can start from either 0th or 1st index
-        return min(climb(cost, n - 1), climb(cost, n - 2));
+        return climb(cost, n);
     }
 };
