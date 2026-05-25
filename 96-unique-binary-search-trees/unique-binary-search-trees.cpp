115b6a47f7e62c3564a6d8f5281cfe20092b9077
Saisha Verma <saishaverma0512@gmail.com>
2026-05-25 07:07:27 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (40.11%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,16 +1,39 @@
+// TOP - DOWN APPROACH : 
 class Solution {
-public:
-    int numTrees(int n) {
-        vector<int> uniqTree(n + 1, 1);
+    vector<int> dp;
+    // dp[i] = no of structurally unique bst which can be formed using i nodes
 
-        for (int nodes = 2; nodes <= n; nodes++) {
-            int total = 0;
-            for (int root = 1; root <= nodes; root++) {
-                total += uniqTree[root - 1] * uniqTree[nodes - root];
-            }
-            uniqTree[nodes] = total;
+    int tree(int n){
+        // base case
+        if (n == 0 || n == 1){
+            return 1;
         }
 
-        return uniqTree[n];        
+        // check dp
+        if (dp[n] != -1){
+            return dp[n];
+        }
+
+        // recursive case
+        int tempans = 0;
+        // we would consider one of these n nodes as root one at a time
+        for (int k = 1; k <= n; k ++){
+            // the left subtree will be formed using (k - 1) nodes
+            int ltree = tree(k - 1);
+            // the right subtree will be formed using (n - k) nodes
+            int rtree = tree(n - k);
+
+            // on counting the combinations each left subtree can be mapped with each of the right subtrees
+            tempans = tempans + (ltree * rtree);
+        }
+
+        return dp[n] = tempans;
+    }
+
+public:
+    int numTrees(int n) {
+        dp.resize(n + 1, -1);
+
+        return tree(n);
     }
 };
