52ec5d5ce5dbe7405dc457765c79834ae91f1aaa
Saisha Verma <saishaverma0512@gmail.com>
2026-05-25 09:42:55 +0530

    Time: 19 ms (78.74%) | Memory: 17.9 MB (80.54%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,39 +1,28 @@
-// TOP - DOWN APPROACH : 
+// BOTTOM - UP APPROACH : 
 class Solution {
-    vector<int> dp;
-    // dp[i] = min no of coins required to make the sum = i
-
-    int change(vector<int> &coins, int amt){
-        // base case
-        if (amt == 0){
-            return 0;
-        }
-
-        // check dp
-        if (dp[amt] != -1){
-            return dp[amt];
-        }
-
-        // recursive case
-        int tempans = INT_MAX;
-        // trying to form the amount from all the possible coin denominations
-        for (int i = 0; i < coins.size(); i ++){
-            if (coins[i] <= amt){
-                // using the ith coint
-                int currans = change(coins, amt - coins[i]);
-                if (currans != INT_MAX){
-                    tempans = min(tempans, currans + 1); // taking the minimum possible answer from all these options
-                }
-            }
-        }
-
-        return dp[amt] = tempans;
-    }
-
 public:
     int coinChange(vector<int>& coins, int amount) {
-        dp.resize(amount + 1, -1);
-        int res = change(coins, amount);
-        return (res == INT_MAX)? -1 : res;
+        int n = coins.size();
+
+        // dp initialization
+        vector<int> dp(amount + 1, -1);
+        dp[0] = 0; // no coins needed to make the total sum = 0
+        // dp[i] = minimum no of coins required to make the sum = i
+
+        // bottom up loop
+        for (int i = 1; i <= amount; i ++){
+            int tempans = INT_MAX;
+
+            // trying to use all the possible coins to make the current sum
+            for (int j = 0; j < n; j ++){
+                if (coins[j] <= i && dp[i - coins[j]] != INT_MAX){
+                    tempans = min(tempans, 1 + dp[i - coins[j]]);
+                }
+            }
+
+            dp[i] = tempans;
+        }
+
+        return (dp[amount] == INT_MAX)? -1 : dp[amount];
     }
 };
