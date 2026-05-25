b0e9f3b4c9dabfb86d10035fd688b401e4fd3e27
Saisha Verma <saishaverma0512@gmail.com>
2026-05-25 12:37:06 +0530

    Time: 20 ms (6.57%) | Memory: 109.2 MB (5.24%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,17 +1,37 @@
+// TOP - DOWN APPROACH : 
 class Solution {
+    vector<int> dp;
+    // dp[i] = max profit obtained considering days from 0 to i
+
+    int profit(vector<int> &prices, int i, int minbuy){
+        // base case
+        if (i == prices.size()){
+            return 0;
+        }
+
+        // check dp
+        if (dp[i] != -1){
+            return dp[i];
+        }
+
+        // recursive case
+        // update the minimum buying price
+        minbuy = min(minbuy, prices[i]);
+
+        // case 1 : skip selling stocks today
+        int op1 = profit(prices, i + 1, minbuy);
+
+        // case 2 : selling the stocks today
+        int op2 = prices[i] - minbuy;
+
+        return dp[i] = max(op1, op2);
+    }
+
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
+        dp.resize(n, -1);
 
-        int maxProf = 0, lastBuy = prices[0];
-        for (int i = 1; i < n; i ++){
-            maxProf = max(maxProf, prices[i] - lastBuy);
-            lastBuy = min(lastBuy, prices[i]);
-        }
-
-        return maxProf;
-        
-        // TC : O(n)
-        // SC : O(1)
+        return profit(prices, 0, prices[0]);
     }
 };
