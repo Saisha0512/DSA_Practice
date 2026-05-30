bf572fbfe66734c037f2ce835ed0a287b1d518b4
Saisha Verma <saishaverma0512@gmail.com>
2026-05-30 06:23:44 +0530

    Time: 319 ms (60.85%) | Memory: 230.9 MB (39.40%) - LeetSync

--- 3437-maximum-total-damage-with-spell-casting/maximum-total-damage-with-spell-casting.cpp
+++ 3437-maximum-total-damage-with-spell-casting/maximum-total-damage-with-spell-casting.cpp
@@ -1,52 +1,38 @@
-// TOP - DOWN APPROACH : 
+// BOTTOM - UP APPROACH : 
 class Solution {
-    vector<long long> dp;
-    // dp[i] = max possible total damage caused while considering the spells till the ith index in val array
-
-    long long cast(vector<long long> &val, unordered_map<long long, long long> &damage, int n){
-        // base case
-        if (n < 0){
-            return 0;
-        }
-
-        // check dp
-        if (dp[n] != -1){
-            return dp[n];
-        }
-
-        // recursive case
-        // case 1 : skip casting the current spell
-        long long op1 = cast(val, damage, n - 1);
-
-        // case 2 : cast the current spell & skip n - 1 & n - 2, jump straight to n - 3
-        long long currpow = val[n];
-        long long op2 = damage[currpow];
-        long long i = lower_bound(val.begin(), val.end(), currpow - 2) - val.begin() - 1;
-        op2 += cast(val, damage, i);
-
-        return dp[n] = max(op1, op2);
-    }
-
 public:
     long long maximumTotalDamage(vector<int>& power) {
-        int n = power.size();
-
-        // calculating the damage caused by all the occurrences of each damage
-        int maxel = *max_element(power.begin(), power.end());
+        // calculating the total damage caused by each unique spell
         unordered_map<long long, long long> damage;
         for (int num : power){
             damage[num] += num;
         }
-        // storing the unique power values of spells in increasing order
+        // storing the values of unique spells in sorted order
         vector<long long> val;
-        for(auto &p : damage){
+        for (auto &p : damage){
             val.push_back(p.first);
         }
         sort(val.begin(), val.end());
 
         // dp initialization
-        dp.resize(val.size(), -1LL);
+        int n = val.size();
+        vector<long long> dp(n, -1);
 
-        return cast(val, damage, val.size() - 1);
+        // bottom up loop
+        for (int i = 0; i < n; i ++){
+            // case 1 : skip casting the current spell
+            long long op1 = (i - 1 >= 0)? dp[i - 1] : 0;
+
+            // case 2 : cast the current spell & skip the 2 adjacent spells
+            long long currpow = val[i];
+            long long op2 = damage[currpow];
+
+            long long j = lower_bound(val.begin(), val.end(), currpow - 2) - val.begin() - 1;
+            op2 += (j >= 0)? dp[j] : 0;
+
+            dp[i] = max(op1, op2);
+        }
+
+        return dp[n - 1];
     }
 };
