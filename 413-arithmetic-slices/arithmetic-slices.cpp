602b55703a78720c0b0caa4830f336f3f41a62e4
Saisha Verma <saishaverma0512@gmail.com>
2026-05-31 01:21:39 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (8.40%) - LeetSync

--- 413-arithmetic-slices/arithmetic-slices.cpp
+++ 413-arithmetic-slices/arithmetic-slices.cpp
@@ -1,39 +1,34 @@
-// TOP - DOWN APPROACH : 
+// BOTTOM - UP APPROACH : 
 class Solution {
-    vector<int> dp;
-    // dp[i] = no of arithmetic subarrays till the index i
-
-    int slice(vector<int> &nums, int n){
-        // base case
-        if (n <= 1){
-            return 0;
-        }
-
-        // check dp
-        if (dp[n] != -1){
-            return dp[n];
-        }
-
-        // recursive case
-        // considering all the previously formed subarrays
-        int tempans = slice(nums, n - 1);
-        if (nums[n] - nums[n - 1] == nums[n - 1] - nums[n - 2]){
-            // all the subarrays which end at the (n - 1)th index & can be extended forward
-            int prev = slice(nums, n - 1) - slice(nums, n - 2);
-
-            // so we add the no of all the arrays which can be extended forwards
-            // and the 1 more 3-length array formed using the elements at n - 2, n - 1 & n
-            tempans += prev + 1;
-        }
-
-        return dp[n] = tempans;
-    }
-
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
         int n = nums.size();
-        dp.resize(n, -1);
+        if (n <= 2){
+            return 0;
+        }
 
-        return slice(nums, n - 1);
+        // dp intialization
+        vector<int> dp(n, -1);
+        // base cases
+        dp[0] = dp[1] = 0;
+
+        // bottom up loop
+        for (int i = 2; i < n; i ++){
+            // considering all the subarrays that ended till the previous index
+            int tempans = dp[i - 1];
+
+            if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]){
+                // computing all the arrays which can be extended till the current ith index from the previous index
+                int prev = dp[i - 1] - dp[i - 2];
+
+                // adding the no of subarrays which can be extended
+                // and the one for the new 3-length window formed using the elements at the index i, i - 1 & i - 2
+                tempans += prev + 1;
+            }
+
+            dp[i] = tempans;
+        }
+
+        return dp[n - 1];
     }
 };
