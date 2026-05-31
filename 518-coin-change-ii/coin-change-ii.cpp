bcb5fd81251d5934993adff0da96ba9138177ee5
Saisha Verma <saishaverma0512@gmail.com>
2026-06-01 04:13:24 +0530

    Time: 39 ms (34.29%) | Memory: 61.8 MB (5.94%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,48 +1,30 @@
-// TOP - DOWN APPROACH : 
 class Solution {
-    vector<vector<int>> dp;
-    // dp[i][amt] = no of combinations that make up the sum = amt using the coins starting from the ith index in the given array
-
-    int coinChange(vector<int> &coins, int amt, int i){
-        // base case
-        if (amt == 0){
-            // one combination found
-            return 1;
-        }
-        if (i == coins.size()){
-            // out of bounds - not possible
-            return 0;
-        }
-
-        // check dp
-        if (dp[i][amt] != -1){
-            return dp[i][amt];
-        }
-
-        // recursive case
-        // case 1 : skip the current coin
-        int op1 = coinChange(coins, amt, i + 1);
-
-        // case 2 : take & add the current coin in the amount
-        int op2 = 0;
-        if (amt >= coins[i]){
-            // as we have infinite no of coins for every denomination, we can start from the same index in the next call
-            op2 = coinChange(coins, amt - coins[i], i);
-        }
-
-        return dp[i][amt] = op1 + op2;
-    }
-
 public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
 
-        // sorting the coins in increasing order
-        sort(coins.begin(), coins.end());
+        vector<vector<unsigned long long>> dp(n + 1, vector<unsigned long long>(amount + 1, 0));
 
-        // dp initialization
-        dp.resize(n, vector<int>(amount + 1, -1));
+        // Base case:
+        // There is exactly 1 way to make amount 0:
+        // choose no coins.
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
 
-        return coinChange(coins, amount, 0);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+
+                // Skip current coin
+                dp[i][j] = dp[i - 1][j];
+
+                // Take current coin
+                if (j >= coins[i - 1]) {
+                    dp[i][j] += dp[i][j - coins[i - 1]];
+                }
+            }
+        }
+
+        return dp[n][amount];
     }
 };
