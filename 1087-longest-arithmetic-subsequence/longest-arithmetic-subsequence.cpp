b8373431b4af5611513b455023fff3f5458fab9d
Saisha Verma <saishaverma0512@gmail.com>
2026-06-05 07:30:21 +0530

    Time: 1153 ms (50.66%) | Memory: 248 MB (44.47%) - LeetSync

--- 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
+++ 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
@@ -3,23 +3,27 @@ class Solution {
 public:
     int longestArithSeqLength(vector<int>& nums) {
         int n = nums.size();
-        // base case
+        // base case 
         if (n <= 2){
             return n;
         }
 
-        // dp initialization
+        // dp intialization
         int ans = 2;
         vector<unordered_map<int, int>> dp(n);
-        // dp[i][diff] = length of longest arithmetic subseq till the ith index with the difference between elements = diff
+        // dp[i][diff] = length of longest arithmetic subseq ending at the ith index with a difference between the elements = diff
 
         // bottom up loop
         for (int i = 0; i < n; i ++){
+            // iterating over all the previous elements
             for (int j = 0; j < i; j ++){
                 int diff = nums[i] - nums[j];
+                // if there exists a subseq that ends t the jth index with the difference between its elements = diff
+                // then we extend that subseq
                 if (dp[j].find(diff) != dp[j].end()){
                     dp[i][diff] = dp[j][diff] + 1;
                 }
+                // otherwise we don't extend the subseq
                 else {
                     dp[i][diff] = 2;
                 }
