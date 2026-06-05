27543478f6ec81b1a5cba9d05ba699c0da977047
Saisha Verma <saishaverma0512@gmail.com>
2026-06-05 14:09:18 +0530

    Time: 452 ms (5.00%) | Memory: 144.6 MB (6.40%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,48 +1,44 @@
-// TOP - DOWN APPROACH : 
+// BOTTOM - UP APPROACH : 
 class Solution {
-    vector<vector<vector<int>>> dp;
-    // dp[i][j][k] = max profit you can achieve starting from index i, when you can buy the stocks atmost j times & sell them atmost k times
-
-    int stocks(vector<int> &prices, int i, int buy, int sell){
-        // base case
-        if (buy == 0 && sell == 0){
-            return 0;
-        }
-        if (i == prices.size()){
-            return 0;
-        }
-
-        // check dp
-        if (dp[i][buy][sell] != -1){
-            return dp[i][buy][sell];
-        }
-
-        // recursive case
-        // case 1 : buy the current stock
-        int op1 = 0;
-        if (buy == sell){ // previous stock has been sold already
-            op1 = - prices[i] + stocks(prices, i + 1, buy - 1, sell);
-        }
-
-        // case 2 : sell the tock at the current day
-        int op2 = 0;
-        if (sell > buy){
-            op2 = prices[i] + stocks(prices, i + 1, buy, sell - 1);
-        }
-
-        // case 3 : skip the current day
-        int op3 = stocks(prices, i + 1, buy, sell);
-
-        return dp[i][buy][sell] = max({op1, op2, op3});
-    }
-
 public:
-    int maxProfit(int k, vector<int>& prices) {
+    int maxProfit(int l, vector<int>& prices) {
         int n = prices.size();
+        const int neg = -1e9;
 
         // dp intialization
-        dp.resize(n, vector<vector<int>>(k + 1, vector<int>(k + 1, -1)));
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(l + 1, vector<int>(l + 1, neg)));
+        // dp base case
+        for (int i = 0; i < n; i ++){
+            dp[i][0][0] = 0;
+        }
+        for (int i = 0; i <= l; i ++){
+            dp[n][i][i] = 0;
+        }
 
-        return stocks(prices, 0, k, k);
+        // bottom up loop
+        for (int i = n - 1; i >= 0; i --){
+            for (int j = 0; j <= l; j ++){
+                for (int k = 1; k <= l; k ++){
+                    // case 1 : buying the stocks at the current day
+                    int op1 = INT_MIN;
+                    if (j == k){
+                        op1 = -prices[i] + dp[i + 1][j - 1][k];
+                    }
+
+                    // case 2 : selling the stocks at the current day
+                    int op2 = INT_MIN;
+                    if (j < k){
+                        op2 = prices[i] + dp[i + 1][j][k - 1];
+                    }
+
+                    // case 3 : skipping the current day
+                    int op3 = dp[i + 1][j][k];
+
+                    dp[i][j][k] = max({op1, op2, op3});
+                }
+            }
+        }
+
+        return dp[0][l][l];
     }
 };
