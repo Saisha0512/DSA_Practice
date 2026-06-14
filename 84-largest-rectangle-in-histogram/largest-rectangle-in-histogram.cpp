1174c0ca0d101cbc524a34ae42eade3ec6359b03
Saisha Verma <saishaverma0512@gmail.com>
2026-06-15 01:29:10 +0530

    Time: 43 ms (9.40%) | Memory: 87.7 MB (14.93%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,21 +1,58 @@
-// OPTIMIZED APPROACH : 
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& heights) {
-        heights.push_back(0);
-        int n = heights.size();
-        stack<int> s;
-        int maxarea = 0;
+    int largestRectangleArea(vector<int>& ht) {
+        int n = ht.size();
 
+        stack<int> s;
+        vector<int> left(n), right(n);
+        // left[i] = max area that can be covered starting from the index i extending towards left
+        // right[i] = max area that cab be extended toward right starting from i
+
+        // calculting the extendable area towards left with the height = ht[i]
         for (int i = 0; i < n; i ++){
-            while (!s.empty() && heights[s.top()] >= heights[i]){
-                int h = heights[s.top()];
+            // popping all the greater elements
+            while (!s.empty() && ht[s.top()] > ht[i]){
                 s.pop();
-                int w = i - ((s.empty())? -1 : s.top()) - 1;
-                maxarea = max(maxarea, h * w);
+            }
+
+            // calculating the area
+            if (s.empty()){
+                left[i] = ht[i] * (i + 1);
+            }
+            else {
+                left[i] = ht[i] * (i - s.top());
             }
 
             s.push(i);
+        }
+
+        // clearing the whole stack
+        while (!s.empty()){
+            s.pop();
+        }
+
+        // calculating the extendable area towards right with the height = ht[i]
+        for (int i = n - 1; i >= 0; i --){
+            // popping all the greater elements
+            while (!s.empty() && ht[s.top()] >= ht[i]){
+                s.pop();
+            }
+
+            // calculating the area
+            if (s.empty()){
+                right[i] = ht[i] * (n - i);
+            }
+            else {
+                right[i] = ht[i] * (s.top() - i);
+            }
+
+            s.push(i);
+        }
+
+        // finding the max possible area with the ht[i]
+        int maxarea = INT_MIN;
+        for (int i = 0; i < n; i ++){
+            maxarea = max(maxarea, left[i] + right[i] - ht[i]);
         }
 
         return maxarea;
