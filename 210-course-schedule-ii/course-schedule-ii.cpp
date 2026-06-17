7fcd7a868de0e47e071e34e0ec78763adef42ed9
Saisha Verma <saishaverma0512@gmail.com>
2026-06-17 20:42:36 +0530

    Time: 1 ms (79.61%) | Memory: 18.6 MB (43.53%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,40 +1,55 @@
 class Solution {
-public:
-    vector<int> findOrder(int n, vector<vector<int>>& pre_req) {
-        // construct a graph
-        vector<vector<int>> graph(n);
-        vector<int> indegree(n, 0);
-        for (auto &e : pre_req){
-            graph[e[1]].push_back(e[0]);
-            indegree[e[0]] ++;
-        }
+    vector<int> order;
+    bool cycle = false;
 
-        // starting with the nodes whose indegree == 0
-        queue<int> q;
-        for (int i = 0; i < n; i ++){
-            if (indegree[i] == 0){
-                q.push(i);
+    void dfs_traversal(vector<vector<int>> &graph, vector<int> &status, int curr_node){
+        // visiting the current node
+        status[curr_node] = 1;
+
+        // iterating over its neighors
+        for (int nbr : graph[curr_node]){
+            // node in the current path
+            if (status[nbr] == 1){
+                cycle = true;
+                return;
             }
-        }
+            else if (status[nbr] == 0){
+                dfs_traversal(graph, status, nbr);
 
-        vector<int> order;
-        // bfs loop over queue
-        while (!q.empty()){
-            int curr_node = q.front();
-            q.pop();
-            order.push_back(curr_node);
-
-            // iterating over its neighbours & removing the edges connecting them
-            for (int nbr : graph[curr_node]){
-                indegree[nbr] --;
-
-                // if the indegree becomes 0, then this node should be push as it is the next node in the priority of pre-requisites
-                if (indegree[nbr] == 0){
-                    q.push(nbr);
+                // if cycle was detected
+                if (cycle){
+                    return;
                 }
             }
         }
 
+        // completing the visit of current node
+        status[curr_node] = 2;
+        order.push_back(curr_node);
+    }
+
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& pre_req) {
+        // constructing the graph
+        vector<vector<int>> graph(n);
+        for (auto &e : pre_req){
+            graph[e[1]].push_back(e[0]);
+        }
+
+        vector<int> status(n, 0);
+        for (int i = 0; i < n; i ++){
+            if (status[i] == 0){
+                dfs_traversal(graph, status, i);
+
+                if (cycle){
+                    // cycle detected - no order possible
+                    return vector<int>();
+                }
+            }
+        }
+
+        // reversing the order to make it correct
+        reverse(order.begin(), order.end());
         return (order.size() == n)? order : vector<int>();
     }
 };
