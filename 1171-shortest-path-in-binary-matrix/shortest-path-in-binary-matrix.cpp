19f09a1f08c5cf9675f164ea6f0cdc43d2009674
Saisha Verma <saishaverma0512@gmail.com>
2026-06-18 15:15:08 +0530

    Time: 10 ms (90.37%) | Memory: 25.4 MB (50.62%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -2,37 +2,40 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
-
-        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1){
+        // base case
+        if (grid[0][0] != 0){
             return -1;
         }
 
-        vector<pair<int, int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {1, 1}, 
-                                        {1, -1}, {-1, 0}, {-1, 1}, {-1, -1}};
+        vector<pair<int, int>> dirs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}};
+
+        vector<vector<int>> dist(n, vector<int>(n, -1));
         queue<pair<int, int>> q;
         q.push({0, 0});
-        grid[0][0] = 1;
-        int len = 1;
+        dist[0][0] = 1;
 
+        // bfs loop
         while (!q.empty()){
-            int size = q.size();
-            while (size --){
-                auto [x, y] = q.front();
-                q.pop();
-                if (x == n - 1 && y == n - 1){
-                    return len;
-                }
+            auto [x, y] = q.front();
+            q.pop();
 
-                for (auto [dx, dy] : dirs){
-                    int i = x + dx, j = y + dy;
-
-                    if (i >= 0 && j >= 0 && i < n && j < n && grid[i][j] == 0){
-                        q.push({i, j});
-                        grid[i][j] = 1;
-                    }
-                }
+            // if the destination is reached
+            if (x == n - 1 && y == n - 1){
+                return dist[x][y];
             }
-            len ++;
+
+            // iterating over the neighbors of the current node
+            for (auto &[dx, dy] : dirs){
+                int i = x + dx, j = y + dy;
+
+                // boundary limits
+                if (i < 0 || j < 0 || i >= n || j >= n || grid[i][j] != 0 || dist[i][j] != -1){
+                    continue;
+                }
+
+                dist[i][j] = dist[x][y] + 1;
+                q.push({i, j});
+            }
         }
 
         return -1;
