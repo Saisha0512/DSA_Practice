f78cf6b2eb440c1ce78444d60c4e8273d21b68ba
Saisha Verma <saishaverma0512@gmail.com>
2026-06-23 03:16:46 +0530

    Time: 2 ms (19.93%) | Memory: 8.9 MB (65.08%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
-        vector<int> freq(26, 0);
+        vector<int> f(5, 0);
+        string res = "balon";
+
         for (char ch : text){
-            freq[ch - 'a'] ++;
+            for (int i = 0; i < 5; i ++){
+                f[i] += ((ch == res[i])? 1 : 0);
+            }
         }
 
-        int min_poss = INT_MAX;
-        string res = "balon";
-        for (char ch : res){
-            if (ch == 'b' || ch == 'a' || ch == 'n'){
-                min_poss = min(min_poss, freq[ch - 'a']);
-            }
-            else {
-                min_poss = min(min_poss, freq[ch - 'a'] / 2);
-            }
-        }
-        
-        return min_poss;
+        return min({f[0], f[1], f[2]/2, f[3]/2, f[4]});
     }
 };
