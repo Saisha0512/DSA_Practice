b77d0a49b93cfcb07b6ecda60a47c7432995426e
Saisha Verma <saishaverma0512@gmail.com>
2026-06-27 21:05:18 +0530

    Time: 35 ms (67.04%) | Memory: 101.2 MB (89.32%) - LeetSync

--- 2139-detect-squares/detect-squares.cpp
+++ 2139-detect-squares/detect-squares.cpp
@@ -1,39 +1,35 @@
 class DetectSquares {
-    unordered_map<string, int> m;
 public:
-    DetectSquares() {
+      map<pair<int,int>,int>mp;
+    DetectSquares() 
+    {
         
     }
-    
-    void add(vector<int> point) {
-        int x = point[0], y = point[1];
-        string key = to_string(x) + "_" + to_string(y);
-        m[key] ++;
+    void add(vector<int> point) 
+    {
+        mp[{point[0],point[1]}]++;
     }
-    
-    int count(vector<int> point) {
-        int cnt = 0;
-        int x = point[0], y = point[1];
-
-        for (auto &p : m){
-            string key = p.first;
-            int pos = key.find('_');
-
-            int i = stoi(key.substr(0, pos)), j = stoi(key.substr(pos + 1));
-
-            // ignore
-            if (i == x || j == y || abs(x - i) != abs(y - j)){
-                continue;
-            }
-
-            string key1 = to_string(x) + "_" + to_string(j); // {x, j}
-            string key2 = to_string(i) + "_" + to_string(y); // {i, y}
-            if (m.count(key1) && m.count(key2)){
-                cnt += (p.second * m[key1] * m[key2]);
+    int count(vector<int> point) 
+    {
+        int count=0;
+        int x1=point[0];
+        int y1=point[1];
+        for(auto it=mp.begin();it!=mp.end();it++){
+            int x2 = (it->first).first;
+            int y2 = (it->first).second;
+            //the first two condition checks if the point coincides
+            // if it does not coincide , then 3rd condition finds a diagnol point of a square .
+            // the 4th and 5th condition finds two points corresponding to that diagnol to form a square. 
+            // if all conditions are satisfied , we muntiply the count of these 3 points and add to count. 
+            if(x1!=x2 && y1!=y2 && abs(x1-x2)==abs(y1-y2) && mp.find({x1,y2})!=mp.end() && mp.find({x2,y1})!=mp.end())
+            {
+                int c1 = mp[{x2,y2}];
+                int c2 = mp[{x2,y1}];
+                int c3 = mp[{x1,y2}];
+                count += c1*c2*c3;
             }
         }
-
-        return cnt;
+        return count;
     }
 };
 
