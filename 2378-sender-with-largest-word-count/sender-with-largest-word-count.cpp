5ed3fca6749dcbc6ff9f71bba40ee1951366776e
Saisha Verma <saishaverma0512@gmail.com>
2026-07-01 02:39:13 +0530

    Time: 30 ms (81.94%) | Memory: 75.9 MB (82.58%) - LeetSync

--- 2378-sender-with-largest-word-count/sender-with-largest-word-count.cpp
+++ 2378-sender-with-largest-word-count/sender-with-largest-word-count.cpp
@@ -1,38 +1,18 @@
 class Solution {
-    int countWords(string s){
-        int cnt = 0;
-        int n = s.size();
-
-        for (char ch : s){
-            if (ch == ' '){
-                cnt ++;
-            }
-        }
-        cnt ++; // last word
-
-        return cnt;
-    }
-
 public:
     string largestWordCount(vector<string>& messages, vector<string>& senders) {
         int n = messages.size();
 
-        unordered_map<string, int> freq;
-        for (int i = 0; i < n; i ++){
-            int curr_cnt = countWords(messages[i]);
-            freq[senders[i]] += curr_cnt;
-        }
-
-        // iterating over the map
         int max_freq = 0;
         string user;
-        for (auto &p : freq){
-            if (p.second > max_freq){
-                max_freq = p.second;
-                user = p.first;
-            }
-            else if (p.second == max_freq && p.first > user){
-                user = p.first;
+        unordered_map<string, int> freq;
+        for (int i = 0; i < n; i ++){
+            int curr_cnt = count(messages[i].begin(), messages[i].end(), ' ') + 1;
+            freq[senders[i]] += curr_cnt;
+
+            if ((freq[senders[i]] > max_freq) || (max_freq == freq[senders[i]] && senders[i] > user)){
+                max_freq = freq[senders[i]];
+                user = senders[i];
             }
         }
 
