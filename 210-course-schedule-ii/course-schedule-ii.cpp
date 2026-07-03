2e70c0a7f8e5afeb4ec12b6973efc6b7d4a2935a
Saisha Verma <saishaverma0512@gmail.com>
2026-07-04 03:57:33 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (50.42%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,36 +1,47 @@
 class Solution {
-public:
-    vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
-        vector<int> indegree(n, 0);
-        vector<vector<int>> graph(n);
-        for (auto &p : prerequisites){
-            graph[p[1]].push_back(p[0]);
-            indegree[p[0]] ++;
+    bool cycle = false;
+    vector<int> order;
+
+    void dfs_traversal(vector<vector<int>> &graph, vector<int> &status, int curr_node){
+        if (cycle){
+            return;
         }
 
-        queue<int> q;
-        for (int i = 0; i < n; i ++){
-            if (indegree[i] == 0){
-                q.push(i);
+        status[curr_node] = 1;
+
+        for (int nbr : graph[curr_node]){
+            if (status[nbr] == 0){
+                dfs_traversal(graph, status, nbr);
+            }
+            else if (status[nbr] == 1){
+                cycle = true;
+                return; // cycle detected
             }
         }
 
-        // kahn's algo
-        vector<int> order;
-        while (!q.empty()){
-            int curr_node = q.front();
-            q.pop();
-            order.push_back(curr_node);
+        status[curr_node] = 2;
+        order.push_back(curr_node);
+    }
 
-            for (int nbr : graph[curr_node]){
-                indegree[nbr] --;
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> graph(n);
+        for (auto &p : prerequisites){
+            graph[p[1]].push_back(p[0]);
+        }
 
-                if (indegree[nbr] == 0){
-                    q.push(nbr);
+        vector<int> status(n, 0);
+        for (int i = 0; i < n; i ++){
+            if (!status[i]){
+                dfs_traversal(graph, status, i);
+
+                if (cycle){
+                    return vector<int>();
                 }
             }
         }
 
+        reverse(order.begin(), order.end());
         return (order.size() == n)? order : vector<int>();
     }
 };
