3ade103cc39c2286f7d8a4b191561ae4f65b597b
Saisha Verma <saishaverma0512@gmail.com>
2026-07-04 04:08:19 +0530

    Time: 49 ms (45.87%) | Memory: 66.2 MB (35.37%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,52 +1,38 @@
 class Solution {
-    bool dfs_traversal(vector<vector<int>> &graph, vector<int> &status, int curr_node){
-        // visiting the current node
-        status[curr_node] = 1;
-
-        // if the current is a safe node
-        // if (graph[curr_node].size() == 0){
-        //     return true;
-        // }
-
-        // otherwise, iterate over the neighbours
-        for (int nbr : graph[curr_node]){
-            // cycle detected - not possible to reach safe node
-            if (status[nbr] == 1){
-                return false;
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        // reversing the edges
+        int n = graph.size();
+        queue<int> q;
+        vector<int> indegree(n, 0);
+        vector<vector<int>> rev_graph(n);
+        for (int i = 0; i < n; i ++){
+            for (int j = 0; j < graph[i].size(); j ++){
+                rev_graph[graph[i][j]].push_back(i);
             }
-            else if (status[nbr] == 0){
-                bool subprob = dfs_traversal(graph, status, nbr);
-                // this traversal can't reach a terminal node
-                if (!subprob){
-                    return false;
+
+            indegree[i] += graph[i].size();
+            if (indegree[i] == 0){
+                q.push(i);
+            }
+        }
+
+        vector<int> res;
+        while (!q.empty()){
+            int curr_node = q.front();
+            q.pop();
+            res.push_back(curr_node);
+
+            for (int nbr : rev_graph[curr_node]){
+                indegree[nbr] --;
+
+                if (indegree[nbr] == 0){
+                    q.push(nbr);
                 }
             }
         }
 
-        // all traversals of this node have reached the terminal node
-        status[curr_node] = 2;
-        return true;
-    }
-
-public:
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size();
-
-        vector<int> status(n, 0);
-        for (int i = 0; i < n; i ++){
-            if (status[i] == 0){
-                dfs_traversal(graph, status, i);
-            }
-        }
-        
-        vector<int> res;
-        // storing the nodes that have all traversals reaching a safe node
-        for (int i = 0; i < n; i ++){
-            if (status[i] == 2){
-                res.push_back(i);
-            }
-        }
-
+        sort(res.begin(), res.end());
         return res;
     }
 };
