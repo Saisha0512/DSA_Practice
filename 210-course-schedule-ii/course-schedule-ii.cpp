ea6683d1c671047512e8445a44fcb36edbece3b7
Saisha Verma <saishaverma0512@gmail.com>
2026-07-04 03:50:59 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (83.22%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,55 +1,36 @@
 class Solution {
-    vector<int> order;
-    bool cycle = false;
-
-    void dfs_traversal(vector<vector<int>> &graph, vector<int> &status, int curr_node){
-        // visiting the current node
-        status[curr_node] = 1;
-
-        // iterating over its neighors
-        for (int nbr : graph[curr_node]){
-            // node in the current path
-            if (status[nbr] == 1){
-                cycle = true;
-                return;
-            }
-            else if (status[nbr] == 0){
-                dfs_traversal(graph, status, nbr);
-
-                // if cycle was detected
-                if (cycle){
-                    return;
-                }
-            }
-        }
-
-        // completing the visit of current node
-        status[curr_node] = 2;
-        order.push_back(curr_node);
-    }
-
 public:
-    vector<int> findOrder(int n, vector<vector<int>>& pre_req) {
-        // constructing the graph
+    vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
+        vector<int> indegree(n, 0);
         vector<vector<int>> graph(n);
-        for (auto &e : pre_req){
-            graph[e[1]].push_back(e[0]);
+        for (auto &p : prerequisites){
+            graph[p[1]].push_back(p[0]);
+            indegree[p[0]] ++;
         }
 
-        vector<int> status(n, 0);
+        queue<int> q;
         for (int i = 0; i < n; i ++){
-            if (status[i] == 0){
-                dfs_traversal(graph, status, i);
+            if (indegree[i] == 0){
+                q.push(i);
+            }
+        }
 
-                if (cycle){
-                    // cycle detected - no order possible
-                    return vector<int>();
+        // kahn's algo
+        vector<int> order;
+        while (!q.empty()){
+            int curr_node = q.front();
+            q.pop();
+            order.push_back(curr_node);
+
+            for (int nbr : graph[curr_node]){
+                indegree[nbr] --;
+
+                if (indegree[nbr] == 0){
+                    q.push(nbr);
                 }
             }
         }
 
-        // reversing the order to make it correct
-        reverse(order.begin(), order.end());
         return (order.size() == n)? order : vector<int>();
     }
 };
