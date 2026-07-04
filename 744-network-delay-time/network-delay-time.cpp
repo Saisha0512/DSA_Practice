1589ed70a09a2a4ce98cf2eeeb488c53f85c63fe
Saisha Verma <saishaverma0512@gmail.com>
2026-07-04 23:28:36 +0530

    Time: 76 ms (83.23%) | Memory: 44.4 MB (53.49%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,39 +1,42 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        // constructing the graph
         vector<vector<pair<int, int>>> graph(n + 1);
         for (auto &e : times){
             graph[e[0]].push_back({e[1], e[2]});
         }
 
         vector<int> dist(n + 1, INT_MAX);
-        queue<int> q;
-        q.push(k);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; // min heap
+        pq.push({k, 0});
         dist[k] = 0;
 
-        while (!q.empty()){
-            int curr_node = q.front();
-            q.pop();
+        while (!pq.empty()){
+            auto [curr_node, time] = pq.top();
+            pq.pop();
 
-            // iterating over the neighbours of this node
+            if (time > dist[curr_node]){
+                continue;
+            }
+
             for (auto &[nbr, wt] : graph[curr_node]){
-                int curr_time = dist[curr_node] + wt;
-                if (dist[nbr] > curr_time){
-                    dist[nbr] = curr_time;
-                    q.push(nbr);
+                if (dist[nbr] > time + wt){
+                    dist[nbr] = time + wt;
+                    pq.push({nbr, dist[nbr]});
                 }
             }
         }
 
-        int max_time = INT_MIN;
+        int min_time = INT_MIN;
         for (int i = 1; i <= n; i ++){
-            if (dist[i] == INT_MAX){ // unreachable node
+            if (dist[i] == INT_MAX){
+                // unreachable node
                 return -1;
             }
-            max_time = max(max_time, dist[i]);
+
+            min_time = max(min_time, dist[i]);
         }
 
-        return max_time;
+        return min_time;
     }
 };
