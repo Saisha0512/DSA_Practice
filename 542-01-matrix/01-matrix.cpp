7572e1c15e65578640d9c2fcd69f030d975c3db7
Saisha Verma <saishaverma0512@gmail.com>
2026-07-04 14:13:21 +0530

    Time: 21 ms (46.37%) | Memory: 34.8 MB (77.00%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -3,31 +3,35 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size(), m = mat[0].size();
 
-        vector<vector<int>> res(n, vector<int>(m, -1));
-        vector<pair<int, int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
         queue<pair<int, int>> q;
+        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+        // pushing all the 0s into the queue
         for (int i = 0; i < n; i ++){
             for (int j = 0; j < m; j ++){
                 if (mat[i][j] == 0){
                     q.push({i, j});
-                    res[i][j] = 0;
+                    dist[i][j] = 0;
                 }
             }
         }
 
+        vector<pair<int, int>> dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
         while (!q.empty()){
             auto [x, y] = q.front();
             q.pop();
 
-            for (auto [dx, dy] : dirs){
+            for (auto &[dx, dy] : dir){
                 int i = x + dx, j = y + dy;
-                if (i >= 0 && j >= 0 && i < n && j < m && res[i][j] == -1){
-                    res[i][j] = res[x][y] + 1;
-                    q.push({i, j});
+
+                if (i < 0 || j < 0 || i >= n || j >= m || dist[i][j] <= dist[x][y] + 1){
+                    continue;
                 }
+
+                dist[i][j] = dist[x][y] + 1;
+                q.push({i, j});
             }
         }
 
-        return res;
+        return dist;
     }
 };
