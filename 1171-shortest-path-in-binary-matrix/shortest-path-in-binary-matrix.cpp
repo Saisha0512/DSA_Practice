911cf36916dc0d245b24d30662afc056d3e6db2a
Saisha Verma <saishaverma0512@gmail.com>
2026-07-05 02:41:12 +0530

    Time: 15 ms (72.24%) | Memory: 24 MB (76.18%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,42 +1,46 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        if (grid[0][0] != 0){
+        if (grid[0][0] == 1){
             return -1;
         }
+
         int n = grid.size();
 
-        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq; // {dist, x, y}
-        pq.push({1, 0, 0});
-        dist[0][0] = 1;
+        queue<pair<int, int>> q;
+        vector<vector<bool>> vis(n, vector<bool>(n, false));
+        q.push({0, 0});
+        vis[0][0] = true;
 
-        vector<pair<int, int>> dir = {{0, 1}, {1, 0}, {1, 1}, {0, -1}, {-1, 0}, {-1, -1}, {-1, 1}, {1, -1}};
+        vector<pair<int, int>> dir = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}};
 
-        while (!pq.empty()){
-            auto curr = pq.top();
-            pq.pop();
-            int curr_dist = curr[0], x = curr[1], y = curr[2];
+        int steps = 1;
+        while (!q.empty()){
+            int size = q.size();
 
-            if (curr_dist > dist[x][y]){
-                continue;
-            }
+            while (size --){
+                auto [x, y] = q.front();
+                q.pop();
 
-            for (auto &[dx, dy] : dir){
-                int i = x + dx, j = y + dy;
-
-                if (i < 0 || j < 0 || i >= n || j >= n || grid[i][j] == 1){
-                    continue;
+                if (x == n - 1 && y == n - 1){
+                    return steps;
                 }
 
-                int new_dist = curr_dist + 1;
-                if (new_dist < dist[i][j]){
-                    dist[i][j] = new_dist;
-                    pq.push({dist[i][j], i, j});
+                for (auto &[dx, dy] : dir){
+                    int i = x + dx, j = y + dy;
+
+                    if (i < 0 || j < 0 || i >= n || j >= n || vis[i][j] || grid[i][j] == 1){
+                        continue;
+                    }
+
+                    q.push({i, j});
+                    vis[i][j] = true;
                 }
             }
+
+            steps ++;
         }
 
-        return (dist[n - 1][n - 1] == INT_MAX)? -1 : dist[n - 1][n - 1];
+        return -1;
     }
 };
