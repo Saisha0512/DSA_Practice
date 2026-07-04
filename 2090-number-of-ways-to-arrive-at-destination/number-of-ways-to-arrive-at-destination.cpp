4fb0522c2a4f99735a6f9585b590ae8758428d35
Saisha Verma <saishaverma0512@gmail.com>
2026-07-04 23:43:40 +0530

    Time: 8 ms (58.79%) | Memory: 35.1 MB (68.87%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -2,44 +2,45 @@ class Solution {
     const int mod = 1e9 + 7;
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        // constructing the graph
         vector<vector<pair<int, int>>> graph(n);
-        for (auto &r : roads){
-            graph[r[0]].push_back({r[1], r[2]});
-            graph[r[1]].push_back({r[0], r[2]});
+        for (auto &e : roads){
+            graph[e[0]].push_back({e[1], e[2]});
+            graph[e[1]].push_back({e[0], e[2]});
         }
 
-        vector<long long> ways(n, 0);
         vector<long long> dist(n, LLONG_MAX);
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+        vector<int> ways(n, 0);
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq; // {dist, node, ways}
         pq.push({0, 0});
         dist[0] = 0;
         ways[0] = 1;
 
-        // bfs loop
         while (!pq.empty()){
             auto [curr_dist, curr_node] = pq.top();
             pq.pop();
             
-            // invalid case
             if (curr_dist > dist[curr_node]){
                 continue;
             }
 
-            // iterating over the neighbors
+            if (curr_node == n - 1){
+                continue;
+            }
+
             for (auto &[nbr, wt] : graph[curr_node]){
                 long long new_dist = curr_dist + wt;
+
                 if (dist[nbr] > new_dist){
                     dist[nbr] = new_dist;
                     ways[nbr] = ways[curr_node];
                     pq.push({dist[nbr], nbr});
                 }
-                else if (new_dist == dist[nbr]){
+                else if (dist[nbr] == new_dist){
                     ways[nbr] = (ways[nbr] + ways[curr_node]) % mod;
                 }
             }
         }
 
-        return ways[n - 1];
+        return ways[n - 1] % mod;
     }
 };
