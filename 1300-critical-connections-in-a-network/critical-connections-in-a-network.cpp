dfbfcb9696a1faa6f2d2082784bd0c7a4a47d5f9
Saisha Verma <saishaverma0512@gmail.com>
2026-07-06 02:48:39 +0530

    Time: 195 ms (51.33%) | Memory: 183.1 MB (35.58%) - LeetSync

--- 1300-critical-connections-in-a-network/critical-connections-in-a-network.cpp
+++ 1300-critical-connections-in-a-network/critical-connections-in-a-network.cpp
@@ -1,32 +1,25 @@
 class Solution {
-    int timer = 0;
-    vector<vector<int>> critical;
-    
-    void dfs_traversal(vector<vector<int>> &graph, vector<int> &time, vector<int> &low, vector<bool> &vis, int curr_node, int parent){
-        // visiting the current node
+    int curr_time = 1;
+    vector<vector<int>> res;
+
+    void dfs_traversal(vector<vector<int>> &graph, vector<bool> &vis, vector<int> &time, vector<int> &low, int curr_node, int parent){
         vis[curr_node] = true;
 
-        // updating the time of visiting the current node
-        time[curr_node] = low[curr_node] = timer;
-        timer ++;
+        low[curr_node] = curr_time;
+        time[curr_node] = curr_time;
+        curr_time ++;
 
-        // iterating over all the neighbors
-        for (auto &nbr : graph[curr_node]){
-            if (nbr == parent)      continue;
-            else if (!vis[nbr]){
-                dfs_traversal(graph, time, low, vis, nbr, curr_node);
+        for (int nbr : graph[curr_node]){
+            if (!vis[nbr]){
+                dfs_traversal(graph, vis, time, low, nbr, curr_node);
 
-                // after this recursive dfs call returns
                 low[curr_node] = min(low[curr_node], low[nbr]);
 
-                // checking if on removing this edge curr_node === nbr
-                // will we be able to reach the nbr from curr_node
-                if (low[nbr] > time[curr_node]){ 
-                    // a critical connections
-                    critical.push_back({curr_node, nbr});
+                if (low[nbr] > time[curr_node]){
+                    res.push_back({curr_node, nbr});
                 }
             }
-            else if (vis[nbr] && nbr != parent){
+            else if (nbr !=  parent){
                 low[curr_node] = min(low[curr_node], low[nbr]);
             }
         }
@@ -34,7 +27,6 @@ class Solution {
 
 public:
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-        // adjacency list
         vector<vector<int>> graph(n);
         for (auto &e : connections){
             graph[e[0]].push_back(e[1]);
@@ -42,9 +34,9 @@ public:
         }
 
         vector<bool> vis(n, false);
-        vector<int> time(n), low(n);
-        dfs_traversal(graph, time, low, vis, 0, -1);
+        vector<int> time(n, -1), low(n, INT_MAX);
+        dfs_traversal(graph, vis, time, low, 0, -1);
 
-        return critical;
+        return res;
     }
 };
