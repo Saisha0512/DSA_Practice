91148e55a52b52898634153183b726f9c1724a0b
Saisha Verma <saishaverma0512@gmail.com>
2026-07-05 22:21:25 +0530

    Time: 9 ms (89.55%) | Memory: 17.4 MB (25.78%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,64 +1,55 @@
 class Solution {
-    int dijkstra(vector<vector<pair<int, int>>> &graph, int n, int node, int threshold){
-        // implementing dijkstra bfs loop starting from curr_node
-        vector<int> dist(n, INT_MAX);
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        pq.push({0, node});
-        dist[node] = 0;
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int threshold) {
+        vector<vector<pair<int, int>>> graph(n);
+        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
+        for (auto &e : edges){
+            graph[e[0]].push_back({e[1], e[2]});
+            graph[e[1]].push_back({e[0], e[2]});
 
-        // bfs loop
-        while (!pq.empty()){
-            auto [curr_dist, curr_node] = pq.top();
-            pq.pop();
+            dist[e[0]][e[1]] = e[2];
+            dist[e[1]][e[0]] = e[2];
+        }
 
-            // if the dist exceed the threshold
-            if (curr_dist > dist[curr_node] || curr_dist > threshold){
-                continue;
-            }
+        // making the dist of node to itself 0
+        for (int i = 0; i < n; i ++){
+            dist[i][i] = 0;
+        }
 
-            // iterating over the neighbors
-            for (auto &[nbr, wt] : graph[curr_node]){
-                int new_dist = curr_dist + wt;
-                if (new_dist < dist[nbr]){
-                    dist[nbr] = new_dist;
-                    pq.push({new_dist, nbr});
+        // trying every node as an intermediate once for every path possible
+        for (int k = 0; k < n; k ++){ // intermediate node
+            for (int i = 0; i < n; i ++){ // start node
+                for (int j = 0; j < n; j ++){ // destination node
+                    // path : i -> k -> j
+
+                    if (dist[i][k] == INT_MAX || dist[k][j] == INT_MAX){
+                        continue;
+                    }
+
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
             }
         }
 
-        // counting the no of nodes within the threshold
-        int cnt = 0;
-        for (int i = 0; i < n; i ++){
-            if (dist[i] <= threshold){
-                cnt ++;
+        // finding the city with min node reachable in threshold
+        int reachable = INT_MAX, res_city = -1;
+        for (int city = 0; city < n; city ++){
+            int cnt = 0;
+            for (int i = 0; i < n; i ++){
+                if (dist[city][i] <= threshold){
+                    cnt ++;
+                }
+            }
+
+            if (cnt < reachable){
+                reachable = cnt;
+                res_city = city;
+            }
+            if (cnt == reachable){
+                res_city = city;
             }
         }
 
-        return cnt - 1; // avoid counting the curr_node itself
-    }
-
-public:
-    int findTheCity(int n, vector<vector<int>>& edges, int threshold) {
-        // construcitng the adjacency list of the graph
-        vector<vector<pair<int, int>>> graph(n);
-        for (auto &e : edges){
-            graph[e[0]].push_back({e[1], e[2]});
-            graph[e[1]].push_back({e[0], e[2]});
-        }
-
-        int max_reachable = INT_MAX, city = 0;
-        for (int i = 0; i < n; i ++){
-            int reachable_cities = dijkstra(graph, n, i, threshold);
-            
-            if (reachable_cities < max_reachable){
-                max_reachable = reachable_cities;
-                city = i;
-            }
-            else if (reachable_cities == max_reachable){
-                city = i;
-            }
-        }
-
-        return city;
+        return res_city;
     }
 };
