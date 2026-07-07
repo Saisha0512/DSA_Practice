2e918f838c4cc2eb405b885ea2469ffbabf92cac
Saisha Verma <saishaverma0512@gmail.com>
2026-07-07 22:47:38 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (99.39%) - LeetSync

--- 833-bus-routes/bus-routes.cpp
+++ 833-bus-routes/bus-routes.cpp
@@ -1,49 +1,49 @@
 class Solution {
 public:
     int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
-        int n = routes.size();
-        // base case
         if (source == target){
             return 0;
         }
 
-        int max_el = INT_MIN;
-        for (auto r : routes){
+        int n = routes.size();
+        int max_stop = INT_MIN;
+        for (auto &r : routes){
             for (int stop : r){
-                max_el = max(max_el, stop);
+                max_stop = max(max_stop, stop);
             }
         }
 
-        // out of bounds
-        if (target > max_el || source > max_el){
+        if (target > max_stop || source > max_stop){
             return -1;
         }
 
-        vector<int> min_stop(max_el + 1, INT_MAX);
-        // min_stop[i] = min no bus changes needed to reach the ith stop on starting from the source
-        min_stop[source] = 0;
+        vector<int> buses(max_stop + 1, INT_MAX);
+        buses[source] = 0;
         bool flag = true;
-
         while (flag){
             flag = false;
 
-            for (auto r : routes){
-                int min_bus = n + 1;
+            for (auto &r : routes){
+                int min_buses = INT_MAX;
                 for (int stop : r){
-                    min_bus = min(min_bus, min_stop[stop]);
+                    min_buses = min(min_buses, buses[stop]);
                 }
-                min_bus ++; // for the current route
 
-                // updating all the stop in the current route that they can be reached via the stop with min buses
+                if (min_buses == INT_MAX){
+                    continue;
+                }
+                
+                min_buses ++; // considering the bus on the current route
+
                 for (int stop : r){
-                    if (min_stop[stop] > min_bus){
-                        min_stop[stop] = min_bus;
-                        flag = true; // done atleast one modification
+                    if (buses[stop] > min_buses){
+                        buses[stop] = min_buses;
+                        flag = true;
                     }
                 }
             }
         }
 
-        return (min_stop[target] < n + 1)? min_stop[target] : -1;
+        return (buses[target] == INT_MAX)? -1 : buses[target];
     }
 };
