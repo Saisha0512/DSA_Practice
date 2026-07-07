a691a72bfe7b21ee96bbd96bb7d2682ccef2caac
Saisha Verma <saishaverma0512@gmail.com>
2026-07-08 02:36:01 +0530

    Time: 112 ms (19.42%) | Memory: 181.8 MB (41.51%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,43 +1,74 @@
+class Node {
+public : 
+    int key;
+    int value;
+    Node *prev, *next;
+
+    Node(int k, int v){
+        this -> key = k;
+        this -> value = v;
+        prev = NULL;
+        next = NULL;
+    }
+};
+
 class LRUCache {
-    list<int> l;
-    unordered_map<int, pair<list<int> :: iterator, int>> mp;
-    int n;
+    int size;
+    Node *head, *tail;
+    unordered_map<int, Node*> mp;
 public:
     LRUCache(int capacity) {
-        this -> n = capacity;
+        size = capacity;
+
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+        head -> next = tail;
+        tail -> prev = head;
     }
 
-    void makeRecentlyUsed(int key){
-        auto it = mp[key].first;
-        l.erase(it);
-        l.push_front(key);
-        mp[key].first = l.begin();
+    void deleteNode(Node *node){
+        Node *p = node -> prev, *n = node -> next;
+        p -> next = n;
+        n -> prev = p;
+        mp.erase(node -> key);
+    }
+    
+    void insertNode(Node *node){
+        Node *n = head -> next;
+        head -> next = node;
+        node -> prev = head;
+        node -> next = n;
+        n -> prev = node;
+        mp[node -> key] = node;
     }
     
     int get(int key) {
         if (mp.count(key)){
-            makeRecentlyUsed(key);
-            return mp[key].second;
+            Node *node = mp[key];
+            deleteNode(node);
+            insertNode(node);
+
+            return node -> value;
         }
 
         return -1;
     }
     
-    void put(int key, int value) {
+    void put(int key, int val) {
         if (mp.count(key)){
-            mp[key].second = value;
-            makeRecentlyUsed(key);
+            Node *node = mp[key];
+            node -> value = val;
+            deleteNode(node);
+            insertNode(node);
         }
         else {
-            if (mp.size() == n){
-                int k = l.back();
-                l.pop_back();
-
-                mp.erase(k);
+            if (mp.size() == size){
+                // delete the last node of dll
+                deleteNode(tail -> prev);
             }
 
-            l.push_front(key);
-            mp[key] = {l.begin(), value};
+            Node *new_node = new Node(key, val);
+            insertNode(new_node);
         }
     }
 };
