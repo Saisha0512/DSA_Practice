4df02d7af007b8f6d985162c0e78712fd9347a32
Saisha Verma <saishaverma0512@gmail.com>
2026-07-08 00:06:43 +0530

    Time: 526 ms (35.31%) | Memory: 11.7 MB (19.06%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,36 +1,49 @@
 class Solution {
+    vector<pair<int, int>> dir = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+
+    bool checkWord(vector<vector<char>> &board, vector<vector<bool>> &vis, string word, int x, int y, int z){
+        int n = board.size(), m = board[0].size();
+        // base case
+        if (z == word.size() - 1 && board[x][y] == word[z]){
+            return true;
+        }
+        if (board[x][y] != word[z]){
+            return false;
+        }
+
+        // recursive case
+        vis[x][y] = true;
+
+        for (auto &[dx, dy] : dir){
+            int i = x + dx, j = y + dy;
+
+            if (i < 0 || j < 0 || i >= n || j >= m || vis[i][j]){
+                continue;
+            }
+
+            bool subprob = checkWord(board, vis, word, i, j, z + 1);
+            if (subprob){
+                return true;
+            }
+        }
+
+        vis[x][y] = false;
+        return false;
+    }
+
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size();
-        int n = board[0].size();
-        
-        function<bool(int, int, int)> backtrack = [&](int i, int j, int k) {
-            if (k == word.length()) {
-                return true;
-            }
-            if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[k]) {
-                return false;
-            }
-            
-            char temp = board[i][j];
-            board[i][j] = '\0';
-            
-            if (backtrack(i + 1, j, k + 1) || backtrack(i - 1, j, k + 1) || 
-                backtrack(i, j + 1, k + 1) || backtrack(i, j - 1, k + 1)) {
-                return true;
-            }
-            
-            board[i][j] = temp; 
-            return false;
-        };
-        
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (backtrack(i, j, 0)) {
+        int n = board.size(), m = board[0].size();
+
+        vector<vector<bool>> vis(n, vector<bool>(m, false));
+        for (int i = 0; i < n; i ++){
+            for (int j = 0; j < m; j ++){
+                if (checkWord(board, vis, word, i, j, 0)){
                     return true;
                 }
             }
         }
+
         return false;
     }
 };
