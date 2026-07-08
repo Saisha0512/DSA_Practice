520563307ad1b5486da789a29d1eee54b81c8057
Saisha Verma <saishaverma0512@gmail.com>
2026-07-09 01:02:30 +0530

    Time: 3 ms (75.47%) | Memory: 19.3 MB (68.23%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,51 +1,35 @@
 class Solution {
-    vector<int> order;
-
-    bool dfs_traversal(vector<vector<int>> &graph, vector<int> &status, int curr_node){
-        // visiting the current node
-        status[curr_node] = 1;
-
-        // iterating over its neighbors
-        for (int nbr : graph[curr_node]){
-            if (status[nbr] == 0){
-                bool subprob = dfs_traversal(graph, status, nbr);
-
-                if (!subprob){
-                    return false;
-                }
-            }
-            else if (status[nbr] == 1){
-                // cycle detected
-                return false;
-            }
-        }
-
-        // push the node in the order
-        order.push_back(curr_node);
-        status[curr_node] = 2;
-        return true;
-    }
-
 public:
-    bool canFinish(int n, vector<vector<int>>& pre_req) {
-        // constructing the graph
+    bool canFinish(int n, vector<vector<int>>& pre) {
+        vector<int> indegree(n, 0);
         vector<vector<int>> graph(n);
-        for (auto &e : pre_req){
+        for (auto &e : pre){
             graph[e[1]].push_back(e[0]);
+            indegree[e[0]] ++;
         }
 
-        // iterating over the bfs loop
-        vector<int> status(n, 0);
+        queue<int> q;
         for (int i = 0; i < n; i ++){
-            if (status[i] == 0){
-                bool subprob = dfs_traversal(graph, status, i);
+            if (!indegree[i]){
+                q.push(i);
+            }
+        }
 
-                if (!subprob){
-                    return false;
+        int cnt = 0;
+        while (!q.empty()){
+            int curr_node = q.front();
+            q.pop();
+            cnt ++;
+
+            for (int nbr : graph[curr_node]){
+                indegree[nbr] --;
+
+                if (indegree[nbr] == 0){
+                    q.push(nbr);
                 }
             }
         }
 
-        return true;
+        return (cnt == n);
     }
 };
