08a2d85aa91a4d66a193133e47bc4419e5b50ab4
Saisha Verma <saishaverma0512@gmail.com>
2026-07-22 18:39:45 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (78.81%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -4,17 +4,23 @@ public:
         int n = grid.size(), m = grid[0].size();
 
         vector<int> dp = grid[0];
-        // taking prefix sum over this
+        // intializing first row of dp by taking prefix sum
         for (int i = 1; i < m; i ++){
             dp[i] += dp[i - 1];
         }
 
-        // bottom up loop
         for (int i = 1; i < n; i ++){
-            dp[0] += grid[i][0];
-            for (int j = 1; j < m; j ++){
-                dp[j] = grid[i][j] + min(dp[j], dp[j - 1]);
+            vector<int> temp(m, INT_MAX);
+
+            for (int j = 0; j < m; j ++){
+                if (j > 0){
+                    temp[j] = min(temp[j], grid[i][j] + temp[j - 1]);
+                }
+
+                temp[j] = min(temp[j], grid[i][j] + dp[j]);
             }
+
+            dp = temp;
         }
 
         return dp[m - 1];
