2b453ab57c9ea408bbac5913ab0f48cd9d2ab31e
Saisha Verma <saishaverma0512@gmail.com>
2026-07-28 03:15:09 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (12.82%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
@@ -18,33 +18,34 @@ public:
         }
 
         long long max_wid = 0;
-
         queue<pair<TreeNode*, long long>> q;
         q.push({root, 0});
         // level order traversal
         while (!q.empty()){
             int size = q.size();
-            long long first = q.front().second, last = 0;
+            long long first, last;
 
             for (int i = 0; i < size; i ++){
                 auto [curr_node, idx] = q.front();
                 q.pop();
-                idx -= first;
 
+                if (i == 0){
+                    first = idx;
+                }
                 if (i == size - 1){
                     last = idx;
                 }
 
                 // inserting the children into the queue
                 if (curr_node -> left){
-                    q.push({curr_node -> left, 2 * idx + 1});
+                    q.push({curr_node -> left, 2 * idx + 1 - first});
                 }
                 if (curr_node -> right){
-                    q.push({curr_node -> right, 2 * idx + 2});
+                    q.push({curr_node -> right, 2 * idx + 2 - first});
                 }
             }
 
-            max_wid = max(max_wid, last + 1);
+            max_wid = max(max_wid, last - first + 1);
         }
 
         return max_wid;
